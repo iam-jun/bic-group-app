88d71a74b4d0ad38dddf8f6a14a1526545eb8c24
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2021-05-20 17:35:34 +0700

    Added hide/show password input

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,15 +1,23 @@
-import iconSearch from "../../assets/icons/icon_search.svg";
-import iconBack from "../../assets/icons/icon_back.svg";
+import iconSearch from '../../assets/icons/icon_search.svg';
+import iconBack from '../../assets/icons/icon_back.svg';
 
 export default {
   iconAdd: {
-    type: "MaterialIcons",
-    name: "add",
+    type: 'MaterialIcons',
+    name: 'add',
   },
   iconMenu: {
-    type: "MaterialIcons",
-    name: "menu",
+    type: 'MaterialIcons',
+    name: 'menu',
   },
   iconSearch,
   iconBack,
+  iconEye: {
+    type: 'MaterialCommunityIcons',
+    name: 'eye-outline',
+  },
+  iconEyeOff: {
+    type: 'MaterialCommunityIcons',
+    name: 'eye-off-outline',
+  },
 };
