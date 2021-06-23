06d014cc7c2b477a957806a69fe0ec7246800634
Huỳnh Phương Khanh <phuongkhanh@evolgroup.vn>
2021-06-23 16:26:36 +0700

    Added Comment components

--- src/screens/Home/PostDetail/index.tsx
+++ src/screens/Home/PostDetail/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import ContentItem from '~/theme/components/List/items/ContentItem';
+import ListView from '~/theme/components/List/ListView';
+import {commentData} from './dummy-comment-data';
 import {post} from './dummy-post-data';
 
 // TODO: need to use redux to get data
@@ -13,7 +15,13 @@ const PostDetailScreen = () => {
       behavior="padding"
       keyboardVerticalOffset={20}
       style={styles.container}>
-      <ContentItem {...post} maxLength={-1} showBackButton={true} />
+      <ListView
+        type="comment"
+        data={commentData}
+        ListHeaderComponent={
+          <ContentItem {...post} maxLength={-1} showBackButton={true} />
+        }
+      />
     </Wrapper>
   );
 };
---
+++ src/theme/components/List/items/CommentItem.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Avatar from '../../Image/Avatar';
+import HorizontalView from '../../Layout/HorizontalView';
+import ThemeView from '../../ThemeView';
+import Text from '~/theme/components/Text';
+import {margin, padding} from '~/theme/configs/spacing';
+import {useBaseHook} from '~/hooks';
+import ListView from '../ListView';
+import {dummyReplies} from '~/screens/Home/PostDetail/dummy-replies';
+import {UserType} from './GroupItem';
+
+export interface Props {
+  style?: StyleProp<ViewStyle>;
+  user: UserType;
+  content: string;
+  replyCount?: number;
+  createdAt?: string;
+}
+
+const CommentItem: React.FC<Props> = ({
+  style,
+  user,
+  content,
+  replyCount,
+  createdAt,
+}) => {
+  const {t} = useBaseHook();
+
+  return (
+    <View style={[styles.container, style]}>
+      <HorizontalView>
+        <Avatar user={user} size={40} />
+        <View style={styles.content}>
+          <TouchableOpacity delayLongPress={1000}>
+            <ThemeView color="gray" style={styles.header}>
+              <Text bold>{user?.fullName}</Text>
+              <Text>{content}</Text>
+            </ThemeView>
+          </TouchableOpacity>
+          <HorizontalView style={styles.bottomBar}>
+            <HorizontalView style={styles.commentReaction}>
+              <Text style={styles.commentReactionItem}>{createdAt}</Text>
+              <TouchableOpacity>
+                <Text style={styles.commentReactionItem} bold>
+                  {t('post:button_like')}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.commentReactionItem} bold>
+                  {t('post:button_reply')}
+                </Text>
+              </TouchableOpacity>
+            </HorizontalView>
+          </HorizontalView>
+        </View>
+      </HorizontalView>
+
+      {/* TODO: need to use another method to display replies */}
+      {/* temporarily using dummy replies */}
+      {replyCount && replyCount > 0 && (
+        <ListView type="comment" data={dummyReplies} style={styles.replies} />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 20,
+    marginVertical: margin.tiny,
+  },
+  content: {
+    flex: 1,
+    marginStart: 6,
+  },
+  header: {
+    borderRadius: 16,
+    paddingHorizontal: padding.large,
+    paddingVertical: padding.small,
+    alignSelf: 'baseline',
+    flex: 0,
+  },
+  bottomBar: {
+    width: '100%',
+    marginStart: margin.large,
+    marginTop: 2,
+  },
+  commentReaction: {
+    alignItems: 'flex-end',
+  },
+  commentReactionItem: {
+    marginEnd: margin.small,
+    fontSize: 11,
+  },
+  replies: {
+    paddingTop: padding.large,
+    marginStart: 44,
+    marginBottom: 6,
+  },
+});
+
+export default CommentItem;
