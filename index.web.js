d03cc38c0336973e361f86302ea061c97d6ccce1
Thu Diep <dieplamminhthu@evolgroup.vn>
2021-06-24 16:28:10 +0700

    Correct AWS path

--- index.web.js
+++ index.web.js
@@ -1,8 +1,8 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './App';
-import Amplify from "aws-amplify";
-import awsconfig from "aws-exports";
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 
 if (module.hot) {
   module.hot.accept();
