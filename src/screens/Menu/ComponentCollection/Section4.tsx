60bd2a7a5bb7e2ca54ac9ad1fd9eb0e19a1f4028
NamAnh <anh.tran3@mservice.com.vn>
2021-07-21 21:35:27 +0700

    BEIN-454 add UI test bottom sheet

--- src/screens/Menu/ComponentCollection/Section4.tsx
+++ src/screens/Menu/ComponentCollection/Section4.tsx
@@ -7,6 +7,7 @@ import Divider from '~/beinComponents/Divider';
 import TextInput from '~/beinComponents/input/TextInput';
 import BottomSheet from '~/beinComponents/BottomSheet';
 import Button from '~/beinComponents/Button';
+import PrimaryItem from '~/beinComponents/list/items/PrimaryItem';
 
 const Section4 = () => {
   const {spacing, colors}: ITheme = useTheme();
@@ -68,7 +69,27 @@ const Section4 = () => {
           modalizeRef={baseSheetRef}
           ContentComponent={
             <View style={{marginHorizontal: spacing?.margin.base}}>
-              <Text>Render Base Bottom Sheet Content</Text>
+              <Text.H5 style={{marginVertical: spacing?.margin.base}}>
+                Privary Type
+              </Text.H5>
+              <PrimaryItem
+                title={'Public'}
+                leftIcon={'Globe'}
+                subTitle={'Group shows to everyone. Learn more'}
+                onPressToggle={action => alert('onPress Toggle: ' + action)}
+              />
+              <PrimaryItem
+                title={'Private'}
+                leftIcon={'Lock'}
+                subTitle={'Group’s content show to only members'}
+                onPressToggle={action => alert('onPress Toggle: ' + action)}
+              />
+              <PrimaryItem
+                title={'Public'}
+                leftIcon={'EyeSlash'}
+                subTitle={'This group is secret. Learn more'}
+                onPressToggle={action => alert('onPress Toggle: ' + action)}
+              />
             </View>
           }>
           <Button.Primary
