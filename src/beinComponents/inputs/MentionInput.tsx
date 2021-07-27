351c74a86d98ee0e19e61cd54766279201679176
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2021-07-27 15:25:07 +0700

    BEIN-452 Styling absolute modal

--- src/beinComponents/inputs/MentionInput.tsx
+++ src/beinComponents/inputs/MentionInput.tsx
@@ -5,49 +5,118 @@ import {
   TextInput,
   TextInputProps,
   FlatList,
+  StyleProp,
+  ViewStyle,
+  TouchableOpacity,
 } from 'react-native';
+import {useTheme} from 'react-native-paper';
+
 import Text from '~/beinComponents/Text';
+import {ITheme} from '~/theme/interfaces';
+import Avatar from '~/beinComponents/Avatar';
+import {IUser} from '~/interfaces/IAuth';
 
 export interface MentionInputProps extends TextInputProps {
+  data: IUser[];
   modalPosition: 'top' | 'bottom';
   showModal: boolean;
+  onPress?: () => void;
+  onChangeText?: (value: string) => void;
+  textInputStyle?: StyleProp<ViewStyle>;
+  modalStyle?: StyleProp<ViewStyle>;
 }
 
-const MentionInput: React.FC<MentionInputProps> = (
-  props: MentionInputProps,
-) => {
-  const [isMentionModalVisible, setMentionModalVisible] = useState<boolean>(
-    props.showModal,
-  );
-  return (
-    <View>
-      {isMentionModalVisible && (
-        // TODO: replace with UI of Mention Modal
+const MentionInput: React.FC<MentionInputProps> = ({
+  data,
+  modalPosition,
+  showModal,
+  textInputStyle,
+  modalStyle,
+  onPress,
+  onChangeText,
+}: MentionInputProps) => {
+  const [isMentionModalVisible, setMentionModalVisible] =
+    useState<boolean>(showModal);
+  const [text, setText] = useState<string>('');
+
+  const theme: ITheme = useTheme();
+  const styles = createStyles(theme, modalPosition);
+
+  const _onChangeText = (text: string) => {
+    setText(text);
+    onChangeText?.(text);
+  };
+
+  const _renderItem = ({item}: {item: IUser}) => {
+    return (
+      <TouchableOpacity onPress={onPress}>
         <View
-          style={styleMentionContainer.containerMention(props.modalPosition)}>
-          <FlatList
-            data={['text1', 'text2', 'text3', 'text4', 'text5']}
-            keyExtractor={item => item}
-            renderItem={({item}) => <Text>{item}</Text>}
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Avatar.Medium
+            style={{
+              marginHorizontal: theme.spacing?.margin.base,
+              marginVertical: theme.spacing?.margin.small,
+            }}
+            source={item.avatarUrl}
           />
+          <Text>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{zIndex: 99}}>
+      {isMentionModalVisible && !!text && (
+        <View style={StyleSheet.flatten([styles.container, modalStyle])}>
+          <FlatList data={data} renderItem={_renderItem} />
         </View>
       )}
-      <TextInput defaultValue={'testValue'} />
+      <TextInput
+        value={text}
+        onChangeText={_onChangeText}
+        placeholder={'Write text to test absolute modal'}
+        style={StyleSheet.flatten([styles.textInputWrapper, textInputStyle])}
+      />
     </View>
   );
 };
 
-const styleMentionContainer = StyleSheet.create({
-  // @ts-ignore
-  containerMention: (position: string) => {
-    return {
-      borderWidth: 1,
+const createStyles = (theme: ITheme, position: string) => {
+  const {colors, spacing} = theme;
+
+  return StyleSheet.create({
+    container: {
       position: 'absolute',
       [position === 'top' ? 'bottom' : 'top']: '100%',
-      left: '14%',
-      width: '75%',
-    };
-  },
-});
+      left: '3%',
+      width: 355,
+      height: 236,
+      borderRadius: 6,
+      backgroundColor: colors.background,
+
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 12,
+      },
+      shadowOpacity: 0.12,
+      shadowRadius: 10.32,
+      elevation: 16,
+    },
+    textInputWrapper: {
+      width: 327,
+      height: 40,
+      borderRadius: 20,
+      backgroundColor: colors.placeholder,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: spacing?.padding.base,
+    },
+  });
+};
 
 export default MentionInput;
--- src/screens/Menu/ComponentCollection/Section1.tsx
+++ src/screens/Menu/ComponentCollection/Section1.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
-import MentionInput from '~/beinComponents/inputs/MentionInput';
 import Text from '~/beinComponents/Text';
 import {useTheme} from 'react-native-paper';
 import {ITheme} from '~/theme/interfaces';
@@ -291,8 +290,6 @@ const Section1 = () => {
 
   return (
     <View style={{}}>
-      <MentionInput modalPosition={'bottom'} showModal={true} />
-
       <Text.H5 style={{margin: spacing?.margin.base}}>Section 1</Text.H5>
       {renderButton()}
       {renderFlashMessage()}
