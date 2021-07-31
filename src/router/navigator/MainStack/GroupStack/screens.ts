c4a1d46e635622010d479fa87f7ec92c9ce63104
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2021-07-31 14:05:59 +0700

    BEIN-422 Added postDetail screen for groupStack

--- src/router/navigator/MainStack/GroupStack/screens.ts
+++ src/router/navigator/MainStack/GroupStack/screens.ts
@@ -1,7 +1,9 @@
 import Groups from '~/screens/Groups';
 import GroupDetail from '~/screens/Groups/GroupDetail';
+import PostDetail from '~/screens/Post/PostDetail';
 
 export default {
   'group-list': Groups,
   'group-detail': GroupDetail,
+  'post-detail': PostDetail,
 };
