9121234c3a7f24ac1bac6f09e838c2a7f4bcbf8a
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-08-02 09:35:27 +0700

    BEIN-614:Add new icons; Replace duplicated

--- src/resources/icons.ts
+++ src/resources/icons.ts
@@ -22,26 +22,55 @@ import iconLanguageEn from '../../assets/icons/icon_language_en.svg';
 import iconLanguageVi from '../../assets/icons/icon_language_vi.svg';
 import iconCheckbox from '../../assets/icons/icon_checkbox.svg';
 import iconTabHome from '../../assets/icons/icon_home.svg';
+import iconTabHomeBein from '../../assets/icons/icon_home_bein.svg';
 import iconTabGroups from '../../assets/icons/icon_groups.svg';
+import iconTabGroupsBein from '../../assets/icons/icon_groups_bein.svg';
 import iconTabChat from '../../assets/icons/icon_chat.svg';
+import iconTabChatBein from '../../assets/icons/icon_chat_bein.svg';
 import iconTabNotification from '../../assets/icons/icon_notification.svg';
+import iconTabNotificationBein from '../../assets/icons/icon_notification_bein.svg';
 import iconTabMenu from '../../assets/icons/icon_menu.svg';
+import iconTabMenuBein from '../../assets/icons/icon_menu_bein.svg';
 import iconCreateChat from '../../assets/icons/icon_create_chat.svg';
+
+// Below are the completely new icons, added on 08/01/2021
+import iconAddPost from '../../assets/icons/icon_add_post.svg';
+import iconArrowSwapHorizontal from '../../assets/icons/icon_arrow_swap_horizontal.svg';
+import iconBill from '../../assets/icons/icon_bill.svg';
+import iconBitcoinConvert from '../../assets/icons/icon_bitcoin_convert.svg';
+import iconBookmark from '../../assets/icons/icon_bookmark.svg';
+import iconBuyCrypto from '../../assets/icons/icon_buy_crypto.svg';
+import iconAddSquareDone from '../../assets/icons/icon_add_square_done.svg';
+import iconAddSquareGroup from '../../assets/icons/icon_add_square_group.svg';
+import iconAddSquareGroupFilled from '../../assets/icons/icon_add_square_gr_filled.svg';
+import iconFeedback from '../../assets/icons/icon_feedback.svg';
+import iconLogout from '../../assets/icons/icon_logout.svg';
+import iconMessageQuestion from '../../assets/icons/icon_message_question.svg';
+import iconMonitor from '../../assets/icons/icon_monitor.svg';
+import iconMoon from '../../assets/icons/icon_moon.svg';
+import iconSetting from '../../assets/icons/icon_setting.svg';
+import iconShieldStar from '../../assets/icons/icon_shield_star.svg';
+import iconWallet from '../../assets/icons/icon_wallet.svg';
 
 const icons = {
   iconTabHome,
+  iconTabHomeBein,
   iconTabGroups,
+  iconTabGroupsBein,
   iconTabChat,
+  iconTabChatBein,
   iconTabNotification,
+  iconTabNotificationBein,
   iconTabMenu,
+  iconTabMenuBein,
   iconAdd: {
     type: 'MaterialIcons',
     name: 'add',
   },
-  iconMenu: {
-    type: 'MaterialIcons',
-    name: 'menu',
-  },
+  // iconMenu: {
+  //   type: 'MaterialIcons',
+  //   name: 'menu',
+  // },
   iconSearch,
   iconBack,
   iconEye: {
@@ -88,10 +117,10 @@ const icons = {
     type: 'MaterialCommunityIcons',
     name: 'diamond-stone',
   },
-  iconNotification: {
-    type: 'MaterialCommunityIcons',
-    name: 'bell-outline',
-  },
+  // iconNotification: {
+  //   type: 'MaterialCommunityIcons',
+  //   name: 'bell-outline',
+  // },
   iconNotificationOff: {
     type: 'MaterialCommunityIcons',
     name: 'bell-off-outline',
@@ -100,10 +129,10 @@ const icons = {
     type: 'MaterialCommunityIcons',
     name: 'magnify',
   },
-  iconChat: {
-    type: 'MaterialCommunityIcons',
-    name: 'chat-outline',
-  },
+  // iconChat: {
+  //   type: 'MaterialCommunityIcons',
+  //   name: 'chat-outline',
+  // },
   iconPin: {
     type: 'MaterialCommunityIcons',
     name: 'pin',
@@ -198,6 +227,23 @@ const icons = {
   iconLanguageVi,
   iconCheckbox,
   iconCreateChat,
+  iconAddPost,
+  iconArrowSwapHorizontal,
+  iconBill,
+  iconBitcoinConvert,
+  iconBookmark,
+  iconBuyCrypto,
+  iconAddSquareGroupFilled,
+  iconAddSquareGroup,
+  iconFeedback,
+  iconLogout,
+  iconMessageQuestion,
+  iconMonitor,
+  iconMoon,
+  iconSetting,
+  iconShieldStar,
+  iconWallet,
+  iconAddSquareDone,
 };
 
 export default icons;
