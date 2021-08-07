d612806daec968697f7cf5a6fbabadea8e9b3149
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-08-07 11:14:10 +0700

    BEIN-547:Fix UI, replace with beinComponents

--- src/screens/Auth/ForgotPassword/components/ForgotInputId.tsx
+++ src/screens/Auth/ForgotPassword/components/ForgotInputId.tsx
@@ -1,17 +1,19 @@
 import React, {useEffect} from 'react';
-import {Container, Image, Text} from '~/components';
-import images from '~/resources/images';
-import {Controller} from 'react-hook-form';
-import TextInput from '~/beinComponents/inputs/TextInput';
-import * as validation from '~/constants/commonRegex';
-import PrimaryButton from '~/components/buttons/PrimaryButton';
-import {useBaseHook} from '~/hooks';
-import {IObject} from '~/interfaces/common';
 import {StyleSheet} from 'react-native';
+import {Controller} from 'react-hook-form';
 import {useTheme} from 'react-native-paper';
 import debounce from 'lodash/debounce';
 import isEmpty from 'lodash/isEmpty';
 import {useDispatch} from 'react-redux';
+
+import images from '~/resources/images';
+import Image from '~/beinComponents/Image';
+import Text from '~/beinComponents/Text';
+import TextInput from '~/beinComponents/inputs/TextInput';
+import Button from '~/beinComponents/Button';
+import * as validation from '~/constants/commonRegex';
+import {useBaseHook} from '~/hooks';
+import {IObject} from '~/interfaces/common';
 import * as actions from '~/screens/Auth/redux/actions';
 import useAuth from '~/hooks/auth';
 import {IForgotPasswordError} from '~/interfaces/IAuth';
@@ -77,49 +79,48 @@ const ForgotInputId: React.FC<Props> = ({useFormData}) => {
         style={styles.logo}
         source={images.logo_bein}
       />
-      <Container fluid style={{flex: 1}}>
-        <Text.H6>{t('auth:text_forgot_password')}</Text.H6>
-        <Text.Body style={styles.desc}>
-          {t('auth:text_forgot_password_input_desc')}
-        </Text.Body>
-        <Controller
-          control={control}
-          render={({field: {onChange, value}}) => (
-            <TextInput
-              testID="inputEmail"
-              label={t('auth:input_label_email')}
-              placeholder={t('auth:input_label_email')}
-              autoCapitalize="none"
-              value={value}
-              editable={!forgotPasswordLoading}
-              error={errors.email}
-              onChangeText={text => {
-                onChange(text);
-                validateEmail();
-              }}
-              helperType={errors.email?.message ? 'error' : undefined}
-              helperContent={errors?.email?.message}
-              style={{marginTop: 0, marginBottom: theme.spacing.margin.small}}
-            />
-          )}
-          rules={{
-            required: t('auth:text_err_email_blank'),
-            pattern: {
-              value: validation.emailRegex,
-              message: t('auth:text_err_email_format'),
-            },
-          }}
-          name="email"
-          defaultValue=""
-        />
-      </Container>
-      <PrimaryButton
+      <Text.H6>{t('auth:text_forgot_password')}</Text.H6>
+      <Text.Body style={styles.desc}>
+        {t('auth:text_forgot_password_input_desc')}
+      </Text.Body>
+      <Controller
+        control={control}
+        render={({field: {onChange, value}}) => (
+          <TextInput
+            testID="inputEmail"
+            label={t('auth:input_label_email')}
+            placeholder={t('auth:input_label_email')}
+            autoCapitalize="none"
+            value={value}
+            editable={!forgotPasswordLoading}
+            error={errors.email}
+            onChangeText={text => {
+              onChange(text);
+              validateEmail();
+            }}
+            helperType={errors.email?.message ? 'error' : undefined}
+            helperContent={errors?.email?.message}
+            style={{marginTop: 0, marginBottom: theme.spacing.margin.small}}
+          />
+        )}
+        rules={{
+          required: t('auth:text_err_email_blank'),
+          pattern: {
+            value: validation.emailRegex,
+            message: t('auth:text_err_email_format'),
+          },
+        }}
+        name="email"
+        defaultValue=""
+      />
+      <Button.Primary
         testID="btnSend"
         disabled={disableRequest}
         loading={forgotPasswordLoading}
-        title={t('auth:btn_send_recover_code')}
         onPress={onRequestForgotPassword}
-      />
+        style={styles.btnSendRecoverCode}>
+        {t('auth:btn_send_recover_code')}
+      </Button.Primary>
     </>
   );
 };
@@ -127,16 +128,19 @@ const ForgotInputId: React.FC<Props> = ({useFormData}) => {
 const themeStyles = (theme: ITheme) => {
   const {spacing, colors} = theme;
   return StyleSheet.create({
-    desc: {
-      marginTop: spacing.margin.tiny,
-      marginBottom: spacing.margin.large,
-      color: colors.textSecondary,
-    },
     logo: {
       alignSelf: 'center',
       width: 64,
       height: 64,
       marginVertical: spacing.margin.big,
+    },
+    desc: {
+      marginTop: spacing.margin.tiny,
+      marginBottom: spacing.margin.large,
+      color: colors.textSecondary,
+    },
+    btnSendRecoverCode: {
+      marginTop: spacing.margin.large,
     },
   });
 };
