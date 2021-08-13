2c02f3e80fd5a8458962939a7e685e67f9825411
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-08-13 11:39:48 +0700

    BEIN-843:Add subTitle to menuItem; Fix styling

--- src/beinComponents/list/items/MenuItem.tsx
+++ src/beinComponents/list/items/MenuItem.tsx
@@ -12,13 +12,24 @@ const MenuItem: React.FC<IOption> = ({
   icon,
   rightSubTitle,
   rightSubIcon,
+  subTitle,
 }: IOption) => {
   const theme = useTheme() as ITheme;
   const styles = themeStyles(theme);
 
   return (
     <View style={styles.container}>
-      <Icon icon={icon} label={title} labelStyle={styles.label} />
+      <Icon icon={icon} size={24} />
+      <View style={styles.titleContainer}>
+        <Text.ButtonBase useI18n style={styles.label}>
+          {title}
+        </Text.ButtonBase>
+        {!!subTitle && (
+          <Text.Subtitle numberOfLines={2} useI18n>
+            {subTitle}
+          </Text.Subtitle>
+        )}
+      </View>
       <View style={styles.rightComponent}>
         {rightSubTitle && (
           <Text.BodyS color={theme.colors.iconTint} useI18n>
@@ -43,14 +54,16 @@ const themeStyles = (theme: ITheme) => {
       backgroundColor: colors.background,
       borderRadius: spacing.borderRadius.base,
       marginHorizontal: spacing.margin.large,
-      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    titleContainer: {
+      flex: 1,
+      marginLeft: spacing.margin.large,
     },
     rightComponent: {
       flexDirection: 'row',
     },
-    label: {
-      marginStart: spacing.margin.extraLarge,
-    },
+    label: {},
     rightSubIcon: {
       marginLeft: spacing.margin.base,
     },
