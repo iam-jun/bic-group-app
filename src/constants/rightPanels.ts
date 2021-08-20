3539ef64d03f02cb5b518f2b1fa275e488976fc8
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-08-20 11:52:55 +0700

    BEIN-794:First add right panel's menu items

---
+++ src/constants/rightPanels.ts
@@ -0,0 +1,26 @@
+export const rightPanelsMenu = [
+  {
+    type: 'aboutBein',
+    title: 'right_panel:about_bein',
+  },
+  {
+    type: 'helpCenter',
+    title: 'right_panel:help_center',
+  },
+  {
+    type: 'privacyTerms',
+    title: 'right_panel:privacy_terms',
+  },
+  {
+    type: 'businessServices',
+    title: 'right_panel:business_services',
+  },
+  {
+    type: 'cryptocurrency',
+    title: 'right_panel:cryptocurrency',
+  },
+  {
+    type: 'more',
+    title: 'right_panel:more',
+  },
+];
