0f9990ef06915412854e0ef904facd0aedd54427
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-08-20 11:51:52 +0700

    BEIN-794:First add right panel image

--- src/resources/images.ts
+++ src/resources/images.ts
@@ -7,6 +7,7 @@ const images = {
   img_user_avatar_default: require('../../assets/images/user_avatar_default.png'),
   img_group_avatar_default: require('../../assets/images/group_avatar_default.png'),
   img_delete: require('../../assets/images/img_delete.png'),
+  img_right_panel: require('../../assets/images/right_panel.png'),
 };
 
 export default images;
