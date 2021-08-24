6d629a26e780ae0466d8165b50344f2e2d34f457
NamAnh <anh.tran3@mservice.com.vn>
2021-08-24 14:13:34 +0700

    [BEIN-558] refactor post detail, add mention to comment input

--- src/screens/Post/PostDetail/index.tsx
+++ src/screens/Post/PostDetail/index.tsx
@@ -4,28 +4,20 @@ import {ITheme} from '~/theme/interfaces';
 import {useTheme} from 'react-native-paper';
 import ScreenWrapper from '~/beinComponents/ScreenWrapper';
 import Header from '~/beinComponents/Header';
-import {usePostDetailReplyingComment} from '~/hooks/post';
-import {IReaction, IRequestPostComment} from '~/interfaces/IPost';
-import CommentInput from '~/beinComponents/inputs/CommentInput';
+import {IAudienceGroup, IReaction} from '~/interfaces/IPost';
 import ListView from '~/beinComponents/list/ListView';
-import postDataHelper from '~/screens/Post/helper/PostDataHelper';
 import CommentItem from '~/beinComponents/list/items/CommentItem';
-import Loading from '~/beinComponents/Loading';
-import Text from '~/beinComponents/Text';
-import {useBaseHook} from '~/hooks';
 import postActions from '~/screens/Post/redux/actions';
 import {useDispatch} from 'react-redux';
-import Icon from '~/beinComponents/Icon';
-import {useUserIdAuth} from '~/hooks/auth';
 import PostView from '~/screens/Post/components/PostView';
 import {useKeySelector} from '~/hooks/selector';
 import postKeySelector from '~/screens/Post/redux/keySelector';
-import Button from '~/beinComponents/Button';
 import {useRootNavigation} from '~/hooks/navigation';
 import {sortComments} from '../helper/PostUtils';
+import CommentInputView from '~/screens/Post/components/CommentInputView';
 
 const PostDetail = (props: any) => {
-  const [commentText, setCommentText] = useState('');
+  const [groupIds, setGroupIds] = useState<string>('');
 
   const params = props?.route?.params;
   const {focusComment} = params || {};
@@ -35,19 +27,15 @@ const PostDetail = (props: any) => {
 
   const dispatch = useDispatch();
   const {rootNavigation} = useRootNavigation();
-  const {t} = useBaseHook();
   const theme: ITheme = useTheme() as ITheme;
   const {colors} = theme;
-  const styles = createStyle(theme);
-
-  const userId = useUserIdAuth();
 
   const id = useKeySelector(postKeySelector.postDetail.id);
   const deleted = useKeySelector(postKeySelector.postDeletedById(id));
+  const audience = useKeySelector(postKeySelector.postAudienceById(id));
   const latest_reactions = useKeySelector(
     postKeySelector.postLatestReactionsComments(id),
   );
-  const replying = usePostDetailReplyingComment();
 
   const comments = useKeySelector(postKeySelector.commentsByParentId(id));
 
@@ -58,53 +46,18 @@ const PostDetail = (props: any) => {
   }, [id]);
 
   useEffect(() => {
-    dispatch(postActions.setPostDetailReplyingComment());
-  }, []);
+    if (audience?.groups?.length > 0) {
+      const ids: any = [];
+      audience.groups.map((g: IAudienceGroup) => ids.push(g?.id));
+      setGroupIds(ids?.join?.(','));
+    }
+  }, [audience?.groups]);
 
   useEffect(() => {
     if (deleted) {
       rootNavigation.goBack();
     }
   }, [deleted]);
-
-  const renderPostContent = () => {
-    if (!id) {
-      return null;
-    }
-    return <PostView postId={id} isPostDetail />;
-  };
-
-  const onTextChange = (text: string) => {
-    setCommentText(text);
-  };
-
-  const onPressSend = () => {
-    if (id) {
-      const commentData = {
-        content: commentText?.trim(),
-      };
-      const replyCmtId = replying?.id;
-      const requestData: IRequestPostComment = {
-        referenceId: replyCmtId || id,
-        referenceType: replyCmtId ? 'comment' : 'post',
-        commentData,
-        userId,
-      };
-      postDataHelper
-        .postNewComment(requestData)
-        .then(response => {
-          if (response && response.data) {
-            setCommentText('');
-            dispatch(postActions.setPostDetailReplyingComment());
-          }
-        })
-        .catch(e => {
-          console.log('\x1b[33m', '🐣️ postNewComment error : ', e, '\x1b[0m');
-        });
-    } else {
-      console.log('\x1b[31m', '🐣️  | onPressSend : invalid id ', '\x1b[0m');
-    }
-  };
 
   const renderCommentItem = ({
     item,
@@ -113,12 +66,10 @@ const PostDetail = (props: any) => {
     item: IReaction;
     index: number;
   }) => {
-    const bgColor = item.id === replying?.id ? colors.bgFocus : undefined;
     return (
       <CommentItem
         postId={id}
         commentData={item}
-        contentBackgroundColor={bgColor}
         onPressReply={(data, isChild) => {
           textInputRef.current?.focus?.();
           if (!isChild) {
@@ -129,43 +80,15 @@ const PostDetail = (props: any) => {
     );
   };
 
-  const renderFooter = () => {
-    return null;
-  };
-
-  const renderCommentInputHeader = () => {
-    if (!replying) {
+  const renderPostContent = () => {
+    if (!id) {
       return null;
     }
-    return (
-      <View style={styles.commentInputHeader}>
-        <View style={{flexDirection: 'row'}}>
-          <Text style={{flex: 1}}>
-            <Text>
-              {t('post:reply_comment_1')}
-              <Text.BodyM>
-                {replying?.user?.data?.fullname || t('post:someone')}
-              </Text.BodyM>
-              <Text>{t('post:reply_comment_2')}</Text>
-            </Text>
-          </Text>
-          <Button
-            onPress={() =>
-              dispatch(postActions.setPostDetailReplyingComment())
-            }>
-            <Text.BodyS>
-              {'• '}
-              <Text.BodyM useI18n color={colors.primary7}>
-                common:btn_cancel
-              </Text.BodyM>
-            </Text.BodyS>
-          </Button>
-        </View>
-        {/*<Text.Subtitle*/}
-        {/*  color={colors.textSecondary}*/}
-        {/*  numberOfLines={1}>{`${replying.data?.content}`}</Text.Subtitle>*/}
-      </View>
-    );
+    return <PostView postId={id} isPostDetail />;
+  };
+
+  const renderFooter = () => {
+    return null;
   };
 
   return (
@@ -181,13 +104,11 @@ const PostDetail = (props: any) => {
         renderItemSeparator={() => <View />}
         keyboardShouldPersistTaps={'handled'}
       />
-      <CommentInput
-        textInputRef={textInputRef}
-        value={commentText}
+      <CommentInputView
+        postId={id}
+        groupIds={groupIds}
         autoFocus={focusComment}
-        onChangeText={onTextChange}
-        onPressSend={onPressSend}
-        HeaderComponent={renderCommentInputHeader()}
+        textInputRef={textInputRef}
       />
     </ScreenWrapper>
   );
@@ -197,10 +118,6 @@ const createStyle = (theme: ITheme) => {
   const {spacing} = theme;
   return StyleSheet.create({
     container: {},
-    commentInputHeader: {
-      marginHorizontal: spacing?.margin.base,
-      marginTop: spacing?.margin.tiny,
-    },
   });
 };
 
