fb9f36fb7782cc2a9366e3f589ebb13025f5f039
Hoang Minh Trong <hoangminhtrong@Hoangs-MacBook-Pro.local>
2021-08-30 18:05:05 +0700

    BEIN-1198 change title of create post, mention in a post notification

--- src/interfaces/IPost.ts
+++ src/interfaces/IPost.ts
@@ -182,7 +182,7 @@ export interface IGetStreamPost {
   verb: string;
   type: string;
   data: IActivityData;
-  object?: string;
+  object?: IGetStreamPost | string;
   origin?: string | null;
   target?: string;
   time?: string;
