8b162147a3f3ceb1582c377a68b6b462360698c2
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-09-02 16:21:18 +0700

    BEIN-1204:Init NoGroupFound.tsx

---
+++ src/screens/Groups/GroupDetail/components/NoGroupFound.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useTheme} from 'react-native-paper';
+import i18next from 'i18next';
+
+import {ITheme} from '~/theme/interfaces';
+import Text from '~/beinComponents/Text';
+import Header from '~/beinComponents/Header';
+import Button from '~/beinComponents/Button';
+import ScreenWrapper from '~/beinComponents/ScreenWrapper';
+import groupStack from '~/router/navigator/MainStack/GroupStack/stack';
+
+const NoGroupFound = () => {
+  const theme = useTheme() as ITheme;
+  const styles = themeStyles(theme);
+  const navigation = useNavigation();
+
+  return (
+    <ScreenWrapper style={styles.root} isFullView>
+      <Header title={i18next.t('error:no_group_found_title')} />
+      <View style={styles.mainContainer}>
+        <Text>{i18next.t('error:no_group_found_desc')}</Text>
+        <Text>{i18next.t('error:no_group_found_second_desc')}</Text>
+        <Button.Primary onPress={() => navigation.navigate(groupStack.groups)}>
+          {i18next.t('error:button_back_to_safety')}
+        </Button.Primary>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+const themeStyles = (theme: ITheme) => {
+  const {colors} = theme;
+  return StyleSheet.create({
+    root: {
+      backgroundColor:
+        Platform.OS === 'web' ? colors.surface : colors.placeholder,
+    },
+    mainContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+};
+
+export default NoGroupFound;
