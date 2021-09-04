739fd4f32334d0429718f4b1b037c469e848cb60
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-09-04 12:15:09 +0700

    BEIN-1148:Add buttons

--- src/beinFragments/NewFeature.tsx
+++ src/beinFragments/NewFeature.tsx
@@ -6,12 +6,27 @@ import {ITheme} from '~/theme/interfaces';
 import Text from '~/beinComponents/Text';
 import SvgIcon from '~/beinComponents/Icon/SvgIcon';
 import NewFeatureImg from '~/../assets/images/new_feeature_grey.svg';
-import i18n from 'i18next';
+import Button from '~/beinComponents/Button';
 
 const NewFeature = () => {
   const theme = useTheme() as ITheme;
   const styles = themeStyle(theme);
 
+  const renderRatingSection = () => {
+    return (
+      <View style={styles.buttonsContainer}>
+        <Button.Secondary useI18n>
+          new_feature:button_I_want_it_now
+        </Button.Secondary>
+        <Button.Secondary style={styles.button} useI18n>
+          new_feature:button_I_can_wait
+        </Button.Secondary>
+        <Button.Secondary style={styles.button} useI18n>
+          new_feature:button_not_really_need
+        </Button.Secondary>
+      </View>
+    );
+  };
   return (
     <View style={styles.root}>
       <SvgIcon
@@ -21,16 +36,15 @@ const NewFeature = () => {
         height={200}
         tintColor="none"
       />
-      <Text.H6>
-        {i18n.t('new_feature:text_we_are_developing_this_feature')}
-      </Text.H6>
-      <Text.Body>{i18n.t('new_feature:text_please_rate_how_urgent')}</Text.Body>
+      <Text.H6 useI18n>new_feature:text_we_are_developing_this_feature</Text.H6>
+      <Text.Body useI18n>new_feature:text_please_rate_how_urgent</Text.Body>
+      {renderRatingSection()}
     </View>
   );
 };
 
 const themeStyle = (theme: ITheme) => {
-  const {colors} = theme;
+  const {colors, spacing} = theme;
 
   return StyleSheet.create({
     root: {
@@ -38,6 +52,12 @@ const themeStyle = (theme: ITheme) => {
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: colors.background,
+    },
+    buttonsContainer: {
+      marginTop: spacing.margin.extraLarge,
+    },
+    button: {
+      marginVertical: spacing.margin.tiny,
     },
   });
 };
