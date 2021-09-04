a2607dfdd07179d43c0b027d5385594950385be9
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-09-04 12:04:35 +0700

    BEIN-1148:Add svg, text

--- src/beinFragments/NewFeature.tsx
+++ src/beinFragments/NewFeature.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useTheme} from 'react-native-paper';
 
-import Text from '~/beinComponents/Text';
 import {ITheme} from '~/theme/interfaces';
+import Text from '~/beinComponents/Text';
+import SvgIcon from '~/beinComponents/Icon/SvgIcon';
+import NewFeatureImg from '~/../assets/images/new_feeature_grey.svg';
+import i18n from 'i18next';
 
 const NewFeature = () => {
   const theme = useTheme() as ITheme;
@@ -11,7 +14,17 @@ const NewFeature = () => {
 
   return (
     <View style={styles.root}>
-      <Text>New feature</Text>
+      <SvgIcon
+        // @ts-ignore
+        source={NewFeatureImg}
+        width={250}
+        height={200}
+        tintColor="none"
+      />
+      <Text.H6>
+        {i18n.t('new_feature:text_we_are_developing_this_feature')}
+      </Text.H6>
+      <Text.Body>{i18n.t('new_feature:text_please_rate_how_urgent')}</Text.Body>
     </View>
   );
 };
