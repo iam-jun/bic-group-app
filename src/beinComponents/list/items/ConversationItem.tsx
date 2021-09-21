4cf6055929c00c416e92c2d2f525f798de39d187
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-09-21 15:26:34 +0700

    BEIN-1481 Add handling render lastMessage in 2 lines

--- src/beinComponents/list/items/ConversationItem.tsx
+++ src/beinComponents/list/items/ConversationItem.tsx
@@ -20,8 +20,13 @@ const ConversationItem: React.FC<IConversation> = ({
   unreadCount,
   type,
 }: IConversation): React.ReactElement => {
+  const AVG_CHAR_ON_ONE_LINE = 32;
+  let twoLineLastMessage = false;
+  if (lastMessage && lastMessage.length >= AVG_CHAR_ON_ONE_LINE)
+    twoLineLastMessage = true;
+
   const theme = useTheme() as ITheme;
-  const styles = createStyles(theme);
+  const styles = createStyles(theme, twoLineLastMessage);
   const {text, textSecondary} = theme.colors;
   const [_avatar, setAvatar] = useState<string | string[] | undefined>(avatar);
   const textColor = unreadCount ? text : textSecondary;
@@ -75,14 +80,17 @@ const ConversationItem: React.FC<IConversation> = ({
           {unreadCount && (
             <RedDot style={styles.unreadBadge} number={unreadCount} />
           )}
+          <RedDot style={styles.unreadBadge} number={20} />
         </View>
       }
     />
   );
 };
 
-const createStyles = (theme: ITheme) => {
+const createStyles = (theme: ITheme, twoLineLastMessage: boolean) => {
   const {spacing} = theme;
+
+  const unreadBadgeMarginTop = !twoLineLastMessage ? 0 : spacing.margin.base;
 
   return StyleSheet.create({
     container: {
@@ -111,7 +119,7 @@ const createStyles = (theme: ITheme) => {
       paddingTop: 0,
     },
     unreadBadge: {
-      marginTop: spacing.margin.base,
+      marginTop: unreadBadgeMarginTop,
     },
   });
 };
