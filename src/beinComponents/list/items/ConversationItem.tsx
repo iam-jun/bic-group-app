e10619300f15166e45db38087003d2101fcee28b
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-09-28 11:23:36 +0700

    BEIN-2311 Create custom conversationItem

--- src/beinComponents/list/items/ConversationItem.tsx
+++ src/beinComponents/list/items/ConversationItem.tsx
@@ -1,16 +1,16 @@
 import i18next from 'i18next';
 import React, {useState} from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import Avatar from '~/beinComponents/Avatar';
 import RedDot from '~/beinComponents/Badge/RedDot';
+import Div from '~/beinComponents/Div';
 import Text from '~/beinComponents/Text';
 import {roomTypes} from '~/constants/chat';
 import {IConversation} from '~/interfaces/IChat';
 import {getDefaultAvatar} from '~/screens/Chat/helper';
 import {ITheme} from '~/theme/interfaces';
 import {countTime, escapeMarkDown} from '~/utils/formatData';
-import PrimaryItem from './PrimaryItem';
 
 const ConversationItem: React.FC<IConversation> = ({
   name,
@@ -20,16 +20,10 @@ const ConversationItem: React.FC<IConversation> = ({
   unreadCount,
   type,
 }: IConversation): React.ReactElement => {
-  const AVG_CHAR_ON_ONE_LINE = 32;
-  let twoLineLastMessage = false;
-  if (lastMessage && lastMessage.length >= AVG_CHAR_ON_ONE_LINE)
-    twoLineLastMessage = true;
-
+  unreadCount = 10;
   const theme = useTheme() as ITheme;
-  const styles = createStyles(theme, twoLineLastMessage);
-  const {text, textSecondary} = theme.colors;
+  const styles = createStyles(theme, unreadCount > 0);
   const [_avatar, setAvatar] = useState<string | string[] | undefined>(avatar);
-  const textColor = unreadCount ? text : textSecondary;
   const isDirect = type === roomTypes.DIRECT;
   const welcomeText =
     type === 'direct'
@@ -42,88 +36,109 @@ const ConversationItem: React.FC<IConversation> = ({
 
   const ItemAvatar = isDirect ? (
     <Avatar.Large
-      style={styles.marginRight}
+      style={styles.avatar}
       source={avatar}
       placeholderSource={getDefaultAvatar(name)}
     />
   ) : (
     <Avatar.Group
       variant="large"
-      style={styles.marginRight}
+      style={styles.avatar}
       source={_avatar}
       onError={onLoadAvatarError}
     />
   );
 
   return (
-    <PrimaryItem
-      title={name}
-      titleProps={{
-        numberOfLines: 1,
-        color: textColor,
-        style: styles.title,
-      }}
-      subTitleProps={{
-        numberOfLines: 2,
-        variant: unreadCount ? 'bodyM' : 'body',
-        color: textColor,
-      }}
-      subTitle={escapeMarkDown(lastMessage) || i18next.t(welcomeText)}
-      style={styles.container}
-      LeftComponent={ItemAvatar}
-      RightComponent={
-        <View style={styles.rightComponent}>
-          <Text.Subtitle
-            style={styles.textUpdate}
-            color={theme.colors.textSecondary}>
-            {countTime(_updatedAt)}
-          </Text.Subtitle>
-          {unreadCount && (
-            <RedDot style={styles.unreadBadge} number={unreadCount} />
-          )}
-        </View>
-      }
-    />
+    <>
+      <Div style={styles.container}>
+        <View>{ItemAvatar}</View>
+        <Div style={styles.contentContainer}>
+          <View style={styles.header}>
+            <Text.H6 style={styles.title} numberOfLines={1}>
+              {name}
+            </Text.H6>
+            <Text.Subtitle
+              style={styles.textUpdate}
+              color={theme.colors.textSecondary}>
+              {countTime(_updatedAt)}
+            </Text.Subtitle>
+          </View>
+          <View style={styles.body}>
+            <Text
+              variant={unreadCount ? 'bodyM' : 'body'}
+              numberOfLines={2}
+              style={styles.lastMessage}>
+              {escapeMarkDown(lastMessage) || i18next.t(welcomeText)}
+            </Text>
+            {!!unreadCount && (
+              <RedDot style={styles.redDot} number={unreadCount} />
+            )}
+          </View>
+          <Div style={styles.divider} />
+        </Div>
+      </Div>
+    </>
   );
 };
 
-const createStyles = (theme: ITheme, twoLineLastMessage: boolean) => {
-  const {spacing} = theme;
-
-  const defaultPaddingTop = spacing.padding.small || 8;
-  const defaultPaddingBottom = spacing.padding.large || 16;
-  const defaultHeight = 60 + defaultPaddingTop + defaultPaddingBottom;
-  const unreadBadgeMarginTop = !twoLineLastMessage ? 0 : spacing.margin.base;
+const createStyles = (theme: ITheme, unreadMessage: boolean) => {
+  const {colors, spacing} = theme;
+  const headerHeight = 20;
+  const bodyHeight = 40;
+  const dividerMarginTop = spacing.margin.small || 8;
+  const dividerHeight = 1;
+  const contentHeight =
+    headerHeight + bodyHeight + dividerMarginTop + dividerHeight * 1.1;
 
   return StyleSheet.create({
     container: {
-      height: defaultHeight,
-      paddingTop: defaultPaddingTop,
-      paddingBottom: defaultPaddingBottom,
-      marginHorizontal: spacing.margin.base,
-      paddingHorizontal: spacing.padding.tiny,
-      alignItems: 'flex-start',
+      flex: 1,
+      flexDirection: 'row',
+      height: 84,
+      paddingVertical: spacing.padding.small,
+      paddingHorizontal: spacing.padding.large,
     },
-    title: {
-      ...Platform.select({
-        web: {
-          paddingTop: 0,
-        },
-      }),
-    },
-    rightComponent: {
-      marginLeft: spacing.margin.base,
-      alignSelf: 'baseline',
-      alignItems: 'flex-end',
-    },
-    marginRight: {
+    avatar: {
       marginRight: spacing.margin.base,
     },
-    textUpdate: {
-      paddingTop: 0,
+    contentContainer: {
+      flex: 1,
+      height: contentHeight,
     },
-    unreadBadge: {
-      marginTop: unreadBadgeMarginTop,
+    header: {
+      flex: 1,
+      flexDirection: 'row',
+      height: '100%',
+      maxHeight: headerHeight,
+    },
+    title: {
+      flex: 1,
+      height: 20,
+    },
+    textUpdate: {
+      height: 16,
+    },
+    body: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: '100%',
+      maxHeight: bodyHeight,
+    },
+    lastMessage: {
+      flex: 1,
+      height: bodyHeight,
+      color: unreadMessage ? colors.textPrimary : colors.textSecondary,
+    },
+    redDot: {
+      marginLeft: spacing.margin.base,
+    },
+    divider: {
+      width: '100%',
+      height: 1,
+      marginTop: dividerMarginTop,
+      backgroundColor: colors.borderDivider,
     },
   });
 };
