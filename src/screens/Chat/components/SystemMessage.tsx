41240975997329bb017be5f5fc8adc8db10cb146
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-10-07 13:36:20 +0700

    BEIN-2534 Add system message type handling

--- src/screens/Chat/components/SystemMessage.tsx
+++ src/screens/Chat/components/SystemMessage.tsx
@@ -1,25 +1,101 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useTheme} from 'react-native-paper';
+import {useDispatch} from 'react-redux';
+
 import Text from '~/beinComponents/Text';
-import {ISystemMessage} from '~/interfaces/IChat';
+import {useRootNavigation} from '~/hooks/navigation';
+import {IMessage} from '~/interfaces/IChat';
+import mainStack from '~/router/navigator/MainStack/stack';
+import modalActions from '~/store/modal/actions';
 import {ITheme} from '~/theme/interfaces';
 
-const SystemMessage: React.FC<ISystemMessage> = ({
-  text,
-  msg,
-}: ISystemMessage) => {
+const SystemMessage: React.FC<IMessage> = (currentMessage: IMessage) => {
   const theme = useTheme() as ITheme;
   const styles = createStyles(theme);
+  const {rootNavigation} = useRootNavigation();
+  const dispatch = useDispatch();
 
-  const content = text?.replace(msg, '');
+  const {text, user, msg} = currentMessage;
 
-  return (
-    <Text.BodyS style={styles.container}>
-      <Text.BodyS style={styles.name}>{msg}</Text.BodyS>
-      {content}
-    </Text.BodyS>
-  );
+  const onPressUser = (username: string) => {
+    const payload = {
+      userId: username,
+      params: {
+        type: 'username',
+      },
+    };
+
+    if (Platform.OS === 'web') {
+      rootNavigation.navigate(mainStack.userProfile, payload);
+    } else {
+      dispatch(modalActions.showUserProfilePreviewBottomSheet(payload));
+    }
+  };
+
+  const renderAddRemoveUser = () => {
+    const content = text?.replace('{1}', '');
+
+    return (
+      <>
+        <TouchableOpacity onPress={() => msg && onPressUser(msg)}>
+          <Text.BodyM style={styles.textBold}>{msg}</Text.BodyM>
+        </TouchableOpacity>
+        {content}
+      </>
+    );
+  };
+
+  const renderChangeDescOrAnnounceOrAvatar = () => {
+    const content = text?.replace('{0}', '');
+
+    return (
+      <>
+        <TouchableOpacity onPress={() => onPressUser(user.username)}>
+          <Text.BodyM style={styles.textBold}>{user.name}</Text.BodyM>
+        </TouchableOpacity>
+        {content}
+      </>
+    );
+  };
+
+  const renderChangeTopicOrChatName = () => {
+    const content = text?.replace(/{(0|1)}/g, '');
+
+    return (
+      <>
+        <TouchableOpacity onPress={() => onPressUser(user.username)}>
+          <Text.BodyM style={styles.textBold}>{user.name}</Text.BodyM>
+        </TouchableOpacity>
+        {content}
+        <Text.BodyM style={styles.textBold}>{msg}</Text.BodyM>
+      </>
+    );
+  };
+
+  const renderContent = () => {
+    /*
+    Switch cases base on message type
+    Too see more types, look up in en.json > chat > system_message
+    */
+    switch (currentMessage.type) {
+      case 'au':
+      case 'ru':
+        return renderAddRemoveUser();
+      case 'room_changed_description':
+      case 'room_changed_announcement':
+      case 'room_changed_avatar':
+        return renderChangeDescOrAnnounceOrAvatar();
+      case 'room_changed_topic':
+      case 'r':
+        return renderChangeTopicOrChatName();
+      default:
+        return text;
+    }
+  };
+
+  return <Text.BodyS style={styles.container}>{renderContent()}</Text.BodyS>;
 };
 
 const createStyles = (theme: ITheme) => {
@@ -30,7 +106,7 @@ const createStyles = (theme: ITheme) => {
       paddingBottom: spacing.padding.small,
       color: colors.textSecondary,
     },
-    name: {
+    textBold: {
       fontWeight: '700',
     },
   });
