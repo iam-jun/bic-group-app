d50efe72561e75c4936974b9c0826f5872cdb098
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2021-10-20 17:05:27 +0700

    BEIN-2676 First add loading current active screen path

--- src/screens/Home/LeftPanel/index.tsx
+++ src/screens/Home/LeftPanel/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Linking, StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
 import PrimaryItem from '~/beinComponents/list/items/PrimaryItem';
@@ -12,6 +12,15 @@ import Text from '~/beinComponents/Text';
 import {useKeySelector} from '~/hooks/selector';
 import postKeySelector from '~/screens/Post/redux/keySelector';
 import {IconType} from '~/resources/icons';
+import {linkingConfigFullLaptop} from '~/configs/navigator';
+
+const homeScreens =
+  linkingConfigFullLaptop.config.screens.MainStack.screens.main.screens;
+
+const PATH = {
+  newsfeed: homeScreens.newsfeed.path,
+  draftPost: homeScreens['draft-post'].path,
+};
 
 const LeftPanel = () => {
   const dispatch = useDispatch();
@@ -19,10 +28,34 @@ const LeftPanel = () => {
   const theme: ITheme = useTheme() as ITheme;
   const styles = createStyle(theme);
 
+  const [currentPath, setCurrentPath] = useState<string>('');
+
   const draftPost = useKeySelector(postKeySelector.draft.posts) || [];
+
+  useEffect(() => {
+    loadCurrentPath();
+  }, []);
+
+  const loadCurrentPath = () => {
+    /**
+     * Currently only load on web, as this component only show on web
+     * Need more work if need to get current active screen on tablet
+     */
+    if (Platform.OS === 'web') {
+      const url = window.location.href;
+
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const parse = require('url-parse');
+      const urlObj = parse(url, true);
+      const path = urlObj.pathname.substr(1);
+      setCurrentPath(path);
+      return;
+    }
+  };
 
   const onPressNewsfeed = () => {
     rootNavigation.navigate(homeStack.newsfeed);
+    setCurrentPath(PATH.newsfeed);
   };
 
   const onPressSavedPosts = () => {
@@ -31,6 +64,7 @@ const LeftPanel = () => {
 
   const onPressDraftPost = () => {
     rootNavigation.navigate(homeStack.draftPost);
+    setCurrentPath(PATH.draftPost);
   };
 
   const renderBadgeNumber = (badgeNumber: number) => {
@@ -48,14 +82,23 @@ const LeftPanel = () => {
   const renderItem = ({
     icon,
     title,
+    path,
     onPress,
     ...props
   }: {
     icon: IconType;
     title: string;
+    path?: string;
     onPress?: () => void;
     [key: string]: any;
   }) => {
+    let style = {};
+    if (path === currentPath) {
+      style = {
+        backgroundColor: theme.colors.placeholder,
+      };
+    }
+
     return (
       <PrimaryItem
         height={48}
@@ -68,21 +111,28 @@ const LeftPanel = () => {
         titleProps={{useI18n: true}}
         title={title}
         onPress={onPress}
+        style={style}
         {...props}
       />
     );
   };
 
   return (
-    <ScreenWrapper testID="VipScreen" disabledDarkMode isFullView>
+    <ScreenWrapper
+      testID="VipScreen"
+      disabledDarkMode
+      isFullView
+      style={styles.root}>
       {renderItem({
         icon: 'iconTabHomeBein',
         title: 'home:newsfeed',
+        path: PATH.newsfeed,
         onPress: onPressNewsfeed,
       })}
       {renderItem({
         icon: 'iconMenuDraft',
         title: 'home:draft_post',
+        path: PATH.draftPost,
         onPress: onPressDraftPost,
         RightComponent: renderBadgeNumber(draftPost?.length || 0),
       })}
@@ -98,6 +148,9 @@ const LeftPanel = () => {
 const createStyle = (theme: ITheme) => {
   const {spacing, colors} = theme;
   return StyleSheet.create({
+    root: {
+      paddingTop: spacing.padding.base,
+    },
     leftIcon: {
       marginLeft: spacing.margin.base,
       marginRight: spacing.margin.extraLarge,
