d163cdc31a7d46324324d4b10e899239926f7b8d
Nhan Nguyen <trinhan@tgm.vn>
2021-11-09 18:13:01 +0700

    BEIN-2671 Fix DownButton styling, add loading indicator

--- src/screens/Chat/components/DownButton.tsx
+++ src/screens/Chat/components/DownButton.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {StyleSheet, Platform, Animated} from 'react-native';
+import {StyleSheet, Platform, Animated, ViewStyle} from 'react-native';
 import {useTheme} from 'react-native-paper';
-import Icon from '~/beinComponents/Icon';
+
 import {ITheme} from '~/theme/interfaces';
+import Icon from '~/beinComponents/Icon';
+import LoadingIndicator from '~/beinComponents/LoadingIndicator';
+import ButtonWrapper from '~/beinComponents/Button/ButtonWrapper';
 
 interface Props {
   visible: boolean;
@@ -12,6 +15,9 @@ interface Props {
 const DownButton = ({visible, onDownPress}: Props) => {
   const theme = useTheme() as ITheme;
   const styles = createStyle(theme);
+  const tintColor = theme.colors.accent;
+
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
 
   const translateYAnimation = useRef(new Animated.Value(0)).current;
   const fadeAnimation = useRef(new Animated.Value(0)).current;
@@ -37,48 +43,68 @@ const DownButton = ({visible, onDownPress}: Props) => {
       toValue: newOpacity,
       useNativeDriver: false,
       duration,
-    }).start();
+    }).start(() => {
+      setIsScrollingDown(false);
+    });
   }, [visible]);
+
+  const onPress = () => {
+    setIsScrollingDown(true);
+    onDownPress();
+  };
+
+  const renderButton = () => {
+    if (isScrollingDown)
+      return <LoadingIndicator size={20} color={tintColor} />;
+
+    return (
+      <ButtonWrapper style={styles.button} onPress={onPress}>
+        <Icon tintColor={tintColor} icon="ArrowDown" />
+      </ButtonWrapper>
+    );
+  };
 
   return (
     <Animated.View
       nativeID="down-button"
       style={[
         styles.container,
-        Platform.OS !== 'web' && styles.shadow,
+        Platform.OS !== 'web' && !isScrollingDown && styles.shadow,
         {
           opacity: fadeAnimation,
           transform: [{translateY: translateYAnimation}],
         },
       ]}
       pointerEvents="box-none">
-      <Icon
-        tintColor={theme.colors.accent}
-        icon="ArrowDown"
-        onPress={onDownPress}
-      />
+      {renderButton()}
     </Animated.View>
   );
 };
 
 const createStyle = (theme: ITheme) => {
   const {colors, spacing} = theme;
+  const size = 40;
+
+  const basicShape: ViewStyle = {
+    width: size,
+    height: size,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: spacing.borderRadius.large,
+  };
+
   return StyleSheet.create({
     container: {
-      width: 40,
-      height: 40,
-      alignItems: 'center',
-      justifyContent: 'center',
+      ...basicShape,
       position: 'absolute',
-      bottom: -40,
+      bottom: -size,
       right: 24,
       borderWidth: 1,
-      padding: spacing.padding.small,
       backgroundColor: colors.background,
       borderColor: Platform.OS !== 'web' ? colors.primary3 : undefined,
-      borderRadius: spacing.borderRadius.large,
     },
     shadow: {
+      ...basicShape,
       shadowColor: '#000',
       shadowOffset: {
         width: 0,
@@ -87,6 +113,10 @@ const createStyle = (theme: ITheme) => {
       shadowOpacity: 0.12,
       shadowRadius: 10.32,
       elevation: 6,
+    },
+    button: {
+      ...basicShape,
+      padding: spacing.padding.small,
     },
   });
 };
