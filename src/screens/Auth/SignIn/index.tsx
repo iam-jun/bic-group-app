b74ac3db2c9d4b8eca26f0d96fbf79264503bfc7
Nhan Nguyen <trinhan@tgm.vn>
2021-11-10 14:48:38 +0700

    BEIN-2813 Add dismiss keyboard when pressing outside fields

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -3,9 +3,11 @@ import React, {useEffect, useRef, useState} from 'react';
 import {Controller, useForm} from 'react-hook-form';
 import {
   Image,
+  Keyboard,
   Platform,
   StyleSheet,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   useWindowDimensions,
   View,
 } from 'react-native';
@@ -143,126 +145,131 @@ const SignIn = () => {
 
   return (
     <ScreenWrapper testID="SignInScreen" style={styles.root} isFullView>
-      <View style={styles.container}>
-        <View>
-          <Image
-            resizeMode="contain"
-            style={styles.logo}
-            source={images.logo_bein}
-          />
-          <Text.H6 style={styles.title} useI18n>
-            auth:text_sign_in_desc
-          </Text.H6>
-          <Controller
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <Input
-                testID="inputEmail"
-                label={
-                  !isWeb && !loading ? t('auth:input_label_email') : undefined
-                }
-                placeholder={
-                  !isWeb ? 'sample@email.com' : t('auth:input_label_email')
-                }
-                keyboardType="email-address"
-                autoCapitalize="none"
-                disabled={loading}
-                value={value}
-                error={errors.email}
-                onChangeText={text => {
-                  onChange(text);
-                  clearFieldError('email');
-                  checkDisableSignIn();
-                }}
-                onSubmitEditing={onSubmitEmail}
-                helperType={errors.email?.message ? 'error' : undefined}
-                helperContent={
-                  errors?.email?.message === signingInError
-                    ? ''
-                    : errors?.email?.message
-                }
-                style={styles.inputEmail}
-              />
-            )}
-            rules={{
-              required: t('auth:text_err_email_blank'),
-              pattern: {
-                value: validation.emailRegex,
-                message: t('auth:text_err_email_format'),
-              },
-            }}
-            name="email"
-          />
-          <Controller
-            control={control}
-            render={({field: {onChange, value}}) => (
-              <PasswordInput
-                testID="inputPassword"
-                ref={inputPasswordRef}
-                label={
-                  !isWeb && !loading
-                    ? t('auth:input_label_password')
-                    : undefined
-                }
-                placeholder={t('auth:input_label_password')}
-                error={errors.password}
-                autoCapitalize="none"
-                disabled={loading}
-                value={value}
-                onChangeText={text => {
-                  onChange(text);
-                  clearFieldError('password');
-                  checkDisableSignIn();
-                }}
-                onSubmitEditing={() => onSignIn()}
-                helperType={errors.password?.message ? 'error' : undefined}
-                helperContent={errors?.password?.message}
-                style={styles.inputPassword}
-              />
-            )}
-            name="password"
-            rules={{
-              required: t('auth:text_err_password_blank'),
-              // min: 8,
-              // max: 20,
-              pattern: {
-                value: validation.passwordRegex,
-                message: t('auth:text_err_password_format'),
-              },
-            }}
-          />
-          <View style={styles.forgotButton}>
+      <TouchableWithoutFeedback
+        onPress={() => Keyboard.dismiss()}
+        accessible={false}
+        style={{flex: 1, backgroundColor: 'pink'}}>
+        <View style={styles.container}>
+          <View>
+            <Image
+              resizeMode="contain"
+              style={styles.logo}
+              source={images.logo_bein}
+            />
+            <Text.H6 style={styles.title} useI18n>
+              auth:text_sign_in_desc
+            </Text.H6>
+            <Controller
+              control={control}
+              render={({field: {onChange, value}}) => (
+                <Input
+                  testID="inputEmail"
+                  label={
+                    !isWeb && !loading ? t('auth:input_label_email') : undefined
+                  }
+                  placeholder={
+                    !isWeb ? 'sample@email.com' : t('auth:input_label_email')
+                  }
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  disabled={loading}
+                  value={value}
+                  error={errors.email}
+                  onChangeText={text => {
+                    onChange(text);
+                    clearFieldError('email');
+                    checkDisableSignIn();
+                  }}
+                  onSubmitEditing={onSubmitEmail}
+                  helperType={errors.email?.message ? 'error' : undefined}
+                  helperContent={
+                    errors?.email?.message === signingInError
+                      ? ''
+                      : errors?.email?.message
+                  }
+                  style={styles.inputEmail}
+                />
+              )}
+              rules={{
+                required: t('auth:text_err_email_blank'),
+                pattern: {
+                  value: validation.emailRegex,
+                  message: t('auth:text_err_email_format'),
+                },
+              }}
+              name="email"
+            />
+            <Controller
+              control={control}
+              render={({field: {onChange, value}}) => (
+                <PasswordInput
+                  testID="inputPassword"
+                  ref={inputPasswordRef}
+                  label={
+                    !isWeb && !loading
+                      ? t('auth:input_label_password')
+                      : undefined
+                  }
+                  placeholder={t('auth:input_label_password')}
+                  error={errors.password}
+                  autoCapitalize="none"
+                  disabled={loading}
+                  value={value}
+                  onChangeText={text => {
+                    onChange(text);
+                    clearFieldError('password');
+                    checkDisableSignIn();
+                  }}
+                  onSubmitEditing={() => onSignIn()}
+                  helperType={errors.password?.message ? 'error' : undefined}
+                  helperContent={errors?.password?.message}
+                  style={styles.inputPassword}
+                />
+              )}
+              name="password"
+              rules={{
+                required: t('auth:text_err_password_blank'),
+                // min: 8,
+                // max: 20,
+                pattern: {
+                  value: validation.passwordRegex,
+                  message: t('auth:text_err_password_format'),
+                },
+              }}
+            />
+            <View style={styles.forgotButton}>
+              <TouchableOpacity
+                testID="btnSignInForgotPassword"
+                onPress={() => navigation.navigate(authStack.forgotPassword)}>
+                <Text.H6 style={styles.transparentButton} useI18n>
+                  auth:btn_forgot_password
+                </Text.H6>
+              </TouchableOpacity>
+            </View>
+            <Button.Primary
+              testID="btnLogin"
+              style={styles.btnSignIn}
+              disabled={disableSignIn}
+              onPress={onSignIn}
+              useI18n>
+              {loading ? <LoadingIndicator /> : 'auth:btn_sign_in'}
+            </Button.Primary>
+          </View>
+          {/*<Text.H5 style={styles.orText} useI18n>auth:text_or</Text.H5>*/}
+          {/*<SignInOAuth />*/}
+          <View style={styles.signUpContainer}>
+            <Text.H6 useI18n>auth:text_sign_up_desc</Text.H6>
             <TouchableOpacity
               testID="btnSignInForgotPassword"
-              onPress={() => navigation.navigate(authStack.forgotPassword)}>
+              // onPress={() => navigation.navigate(authStack.signup)}
+              onPress={handleSignUpNotFunctioning}>
               <Text.H6 style={styles.transparentButton} useI18n>
-                auth:btn_forgot_password
+                auth:btn_sign_up_now
               </Text.H6>
             </TouchableOpacity>
           </View>
-          <Button.Primary
-            testID="btnLogin"
-            style={styles.btnSignIn}
-            disabled={disableSignIn}
-            onPress={onSignIn}
-            useI18n>
-            {loading ? <LoadingIndicator /> : 'auth:btn_sign_in'}
-          </Button.Primary>
         </View>
-        {/*<Text.H5 style={styles.orText} useI18n>auth:text_or</Text.H5>*/}
-        {/*<SignInOAuth />*/}
-        <View style={styles.signUpContainer}>
-          <Text.H6 useI18n>auth:text_sign_up_desc</Text.H6>
-          <TouchableOpacity
-            testID="btnSignInForgotPassword"
-            // onPress={() => navigation.navigate(authStack.signup)}
-            onPress={handleSignUpNotFunctioning}>
-            <Text.H6 style={styles.transparentButton} useI18n>
-              auth:btn_sign_up_now
-            </Text.H6>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </TouchableWithoutFeedback>
     </ScreenWrapper>
   );
 };
@@ -283,6 +290,7 @@ const themeStyles = (theme: ITheme, isPhone: boolean) => {
       justifyContent: !isPhone ? 'center' : undefined,
     },
     container: {
+      flex: 1,
       alignContent: 'center',
       width: '100%',
       maxWidth: 375,
