5651279f9dbb823bffca4b79bf2a4a9a9c782616
Nguyen Anh Thien <thienna@Nguyens-MacBook-Pro.local>
2021-11-19 18:08:04 +0700

    BEIN-3631 push noti ios

--- src/store/index.ts
+++ src/store/index.ts
@@ -22,7 +22,7 @@ const persistConfig = {
   // transforms: [immutableTransform()],
   // transforms: [transformCircular],
   storage: AsyncStorage,
-  blacklist: ['modal', 'groups', 'chat', 'post', 'home', 'notifications'], // temporary block chat until it's stable
+  blacklist: ['modal', 'groups', 'chat', 'post', 'home'], // temporary block chat until it's stable
   // whitelist: ['chat', 'language'],
 };
 
