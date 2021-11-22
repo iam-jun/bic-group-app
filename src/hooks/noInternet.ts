6ebb1597f9b5452630a2d3de44e45cb0f9dda7ec
Nguyen Tri Nhan <trinhan@gmail.com>
2021-11-22 15:47:08 +0700

    BEIN-2886 Remove logging

--- src/hooks/noInternet.ts
+++ src/hooks/noInternet.ts
@@ -3,7 +3,6 @@ import {IObject} from '~/interfaces/common';
 
 const useNoInternet = () => {
   const noInternet = useSelector((state: IObject<any>) => state.noInternet);
-  console.log(`noInternet`, noInternet);
 
   return noInternet;
 };
