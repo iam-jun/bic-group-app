2c96cfb85eae4fa403b61b6e7fd3295e1c4f9191
Nguyen Tri Nhan <trinhan@gmail.com>
2021-11-23 17:51:12 +0700

    BEIN-2886 Fix typo

--- src/screens/NoInternet/redux/types.ts
+++ src/screens/NoInternet/redux/types.ts
@@ -1,4 +1,4 @@
-export const SET_NO_INTERNET = 'noInternet/SET_NO_INTER_NET';
+export const SET_NO_INTERNET = 'noInternet/SET_NO_INTERNET';
 
 export const SET_SYSTEM_ISSUE = 'noInternet/SET_SYSTEM_ISSUE';
 export const SHOW_SYSTEM_ISSUE = 'noInternet/SHOW_SYSTEM_ISSUE';
