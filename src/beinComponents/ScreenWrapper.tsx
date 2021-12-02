69af5455bca413c756a7dc39630960e2d5e42a2c
Nguyen Tri Nhan <trinhan@gmail.com>
2021-12-02 17:19:10 +0700

    BEIN-3573 Remove as cannot press with pointer event

--- src/beinComponents/ScreenWrapper.tsx
+++ src/beinComponents/ScreenWrapper.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
 import {useTheme} from 'react-native-paper';
-import {useKeySelector} from '~/hooks/selector';
 import {ITheme} from '~/theme/interfaces';
 export interface ScreenWrapperProps extends ViewStyle {
   backgroundColor?: string;
@@ -16,14 +15,11 @@ const ScreenWrapper: React.FC<ScreenWrapperProps> = ({
   style,
   ...props
 }: ScreenWrapperProps) => {
-  const isInternetReachable = useKeySelector('noInternet.isInternetReachable');
-
   const theme = useTheme() as ITheme;
 
   return (
     <View
       {...props}
-      pointerEvents={!isInternetReachable ? 'auto' : 'none'}
       style={StyleSheet.flatten([
         {backgroundColor: backgroundColor || theme.colors.background},
         isFullView && {flex: 1},
