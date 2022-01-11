aa295b5140cf7018840b6a722bea5e1e439d3bd9
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2022-01-11 11:10:24 +0700

    [BEIN-4228][GROUP] Updated UI EmptyScreen when data is empty

--- src/beinFragments/EmptyScreen.tsx
+++ src/beinFragments/EmptyScreen.tsx
@@ -2,27 +2,32 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import Text from '~/beinComponents/Text';
-import NoPendingMembersImg from '~/../assets/images/no_pending_members.svg';
+import ImageAddUsers from '~/../assets/images/img_add_users.svg';
 import SvgIcon from '~/beinComponents/Icon/SvgIcon';
 import {ITheme} from '~/theme/interfaces';
 import {useTheme} from 'react-native-paper';
 
-const NoPendingMembers = () => {
-  const theme: ITheme = useTheme() as ITheme;
+interface EmptyScreenProps {
+  title: string;
+  description: string;
+}
+
+const EmptyScreen = ({title, description}: EmptyScreenProps) => {
+  const theme = useTheme() as ITheme;
   const styles = themeStyles(theme);
 
   return (
     <View style={styles.container}>
       {/* @ts-ignore */}
-      <SvgIcon source={NoPendingMembersImg} width={120} height={120} />
+      <SvgIcon source={ImageAddUsers} width={150} height={150} />
       <Text.ButtonBase style={styles.text} useI18n>
-        {'groups:text_no_pending_members_notice'}
+        {title}
       </Text.ButtonBase>
       <Text.Subtitle
         color={theme.colors.textSecondary}
         style={styles.text}
         useI18n>
-        {'groups:text_pending_request_notice'}
+        {description}
       </Text.Subtitle>
     </View>
   );
@@ -44,4 +49,4 @@ const themeStyles = (theme: ITheme) => {
   });
 };
 
-export default NoPendingMembers;
+export default EmptyScreen;
