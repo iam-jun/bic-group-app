8508fcd991de0f773f0e459b1d50290a94dca6d3
NamAnh <anh.tran3@mservice.com.vn>
2022-01-26 14:05:19 +0700

    [BEIN-4289] update test component divider

--- src/beinComponents/Divider.test.js
+++ src/beinComponents/Divider.test.js
@@ -1,16 +1,25 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
+import {render, cleanup} from '@testing-library/react-native';
+
 import Divider from '~/beinComponents/Divider';
+
+afterEach(cleanup);
 
 describe('Divider component', () => {
   it(`renders correctly`, () => {
-    const tree = renderer.create(<Divider />);
-    expect(tree).toMatchSnapshot();
+    const rendered = render(<Divider />).toJSON();
+    expect(rendered).toMatchSnapshot();
   });
+
+  it(`renders correctly color`, () => {
+    const {getByTestId} = render(<Divider color={'#B2BDCD'} />);
+    const dividerComponent = getByTestId('divider');
+    expect(dividerComponent.props.style[0].backgroundColor).toBe('#B2BDCD');
+  });
+
   it(`renders correctly horizontal`, () => {
-    const tree = renderer.create(
-      <Divider horizontal={true} color={'#B2BDCD'} />,
-    );
-    expect(tree).toMatchSnapshot();
+    const {getByTestId} = render(<Divider horizontal={true} />);
+    const dividerComponent = getByTestId('divider');
+    expect(dividerComponent.props.style[0].height).toBe(undefined);
   });
 });
