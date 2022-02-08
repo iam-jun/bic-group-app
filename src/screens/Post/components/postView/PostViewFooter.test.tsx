06bfd72531ad02d34c4c117a77e13d4179291252
NamAnh <anh.tran3@mservice.com.vn>
2022-02-09 00:05:56 +0700

    [BEIN-4289] add component test for newsfeed search

--- src/screens/Post/components/postView/PostViewFooter.test.tsx
+++ src/screens/Post/components/postView/PostViewFooter.test.tsx
@@ -1,58 +1,17 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import configureStore from 'redux-mock-store';
-import {render, cleanup, fireEvent} from '@testing-library/react-native';
 import PostViewFooter from '~/screens/Post/components/postView/PostViewFooter';
+import {cleanup, renderWithRedux, store} from '../../../../../test/testUtils';
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup();
+  store.clearActions();
+});
 
 describe('PostViewFooter component', () => {
-  const mockStore = configureStore([]);
-
   it('should render correctly', () => {
-    const store = mockStore({count: 5});
-    const rendered = render(
-      <Provider store={store}>
-        <PostViewFooter labelButtonComment={'Comment'} reactionCounts={{}} />
-      </Provider>,
+    const rendered = renderWithRedux(
+      <PostViewFooter labelButtonComment={'Comment'} reactionCounts={{}} />,
     );
     expect(rendered.toJSON()).toMatchSnapshot();
   });
-
-  // it('should fire onAddReaction event', () => {
-  //   const store = mockStore({count: 5});
-  //   const onAddReaction = jest.fn();
-  //   const rendered = render(
-  //     <Provider store={store}>
-  //       <PostViewFooter
-  //         labelButtonComment={'Comment'}
-  //         reactionCounts={{}}
-  //         onAddReaction={onAddReaction}
-  //         btnReactTestID={'btnReactTestID'}
-  //       />
-  //     </Provider>,
-  //   );
-  //   const btnReact = rendered.getByTestId('btnReactTestID');
-  //   fireEvent(btnReact, 'onPress');
-  //   expect(onAddReaction).toHaveBeenCalled();
-  // });
-  //
-  // it('should fire onPressComment event', () => {
-  //   const store = mockStore({count: 5});
-  //   const onPressComment = jest.fn();
-  //   const rendered = render(
-  //     <Provider store={store}>
-  //       <PostViewFooter
-  //         labelButtonComment={'Comment'}
-  //         onPressComment={onPressComment}
-  //         btnCommentTestID={'btnCommentTestID'}
-  //         reactionCounts={{}}
-  //       />
-  //     </Provider>,
-  //   );
-  //   const btnReact = rendered.getByTestId('btnCommentTestID');
-  //   fireEvent(btnReact, 'onClick');
-  //   expect(onPressComment).toHaveBeenCalled();
-  // });
-  //
 });
