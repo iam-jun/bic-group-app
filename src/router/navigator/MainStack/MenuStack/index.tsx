19cd6a6b4f9be0d9008d3b98dcba7058b810beee
Linh Thann <duylinh@tgm.vn>
2022-02-09 13:23:30 +0700

    [BEIN-4260][Community][FE][APP]UI Polishment] update menu stack

--- src/router/navigator/MainStack/MenuStack/index.tsx
+++ src/router/navigator/MainStack/MenuStack/index.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import BaseStackNavigator from '~/router/components/BaseStackNavigator';
 import screens from './screens';
 import stack from './stack';
-import {Platform} from 'react-native';
 
 const MenuStack = () => {
-  return (
-    <BaseStackNavigator
-      stack={stack}
-      screens={screens}
-      initialRouteName={Platform.OS === 'web' ? undefined : 'drawer-menu'}
-    />
-  );
+  return <BaseStackNavigator stack={stack} screens={screens} />;
 };
 
 export default MenuStack;
