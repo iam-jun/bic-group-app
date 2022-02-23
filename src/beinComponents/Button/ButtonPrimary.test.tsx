5f1de581f6251574099942037830320dabb93c42
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2022-02-23 09:04:23 +0700

    [BEIN-4493] Added unit test for ButtonPrimary component

---
+++ src/beinComponents/Button/ButtonPrimary.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {renderWithRedux, cleanup} from '~/test/testUtils';
+import ButtonPrimary from './ButtonPrimary';
+
+afterEach(cleanup);
+
+describe('ButtonPrimary conponent', () => {
+  const children = 'Button Primary';
+
+  const testID = 'button_primary.test';
+
+  it('renders correctly', () => {
+    const rendered = renderWithRedux(
+      <ButtonPrimary testID={testID}>{children}</ButtonPrimary>,
+    ).toJSON();
+
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('renders color correctly', () => {
+    const {getByTestId} = renderWithRedux(
+      <ButtonPrimary color="red" testID={testID}>
+        {children}
+      </ButtonPrimary>,
+    );
+
+    const component = getByTestId(testID);
+
+    expect(component.props.style.backgroundColor).toBe('red');
+  });
+
+  it('renders textColor correctly', () => {
+    const {getByTestId} = renderWithRedux(
+      <ButtonPrimary textColor="red" testID={testID}>
+        {children}
+      </ButtonPrimary>,
+    );
+
+    const component = getByTestId(testID);
+
+    expect(component.props.children[0].props.children[2].props.color).toBe(
+      'red',
+    );
+  });
+});
