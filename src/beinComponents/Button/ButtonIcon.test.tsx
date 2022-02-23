a1f9f31320cb3de5e6d9b1cac33c02268925d3e4
ThuQuyenBein <thuquyen@tgm.vn>
2022-02-23 18:15:11 +0700

    BEIN-4492 add tc of button icon and update tc of other

---
+++ src/beinComponents/Button/ButtonIcon.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {renderWithRedux, cleanup, fireEvent} from '~/test/testUtils';
+import {colors} from '~/theme';
+
+import ButtonIcon from './ButtonIcon';
+
+afterEach(cleanup);
+
+describe('ButtonDanger conponent', () => {
+  const children = 'Button Icon';
+
+  const testID = 'button_icon.test';
+
+  const icon = 'iconSend';
+
+  const label = 'BUTTON ICON';
+
+  it('renders correctly', () => {
+    const rendered = renderWithRedux(
+      <ButtonIcon icon={icon} testID={testID} />,
+    ).toJSON();
+
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('renders label correctly', () => {
+    const rendered = renderWithRedux(
+      <ButtonIcon icon={icon} label={label} testID={testID} />,
+    );
+    const labelComponent = rendered.getByTestId('button_icon.label');
+
+    expect(labelComponent).toBeDefined();
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders style correctly', () => {
+    const {getByTestId} = renderWithRedux(
+      <ButtonIcon icon={icon} style={{margin: 12}} testID={testID} />,
+    );
+
+    const component = getByTestId('button_icon');
+
+    expect(component.props.style[1].margin).toBe(12);
+  });
+
+  it('renders iconWrapperStyle correctly', () => {
+    const rendered = renderWithRedux(
+      <ButtonIcon
+        icon={icon}
+        iconWrapperStyle={{backgroundColor: 'red'}}
+        testID={testID}
+      />,
+    );
+
+    const component = rendered.getByTestId(testID);
+
+    expect(component.props.style.backgroundColor).toBe('red');
+  });
+});
