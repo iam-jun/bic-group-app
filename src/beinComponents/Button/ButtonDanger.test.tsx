3ba00021dda001fa4a38303880a2dbc98e63ebcc
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2022-02-23 10:12:02 +0700

    [BEIN-4495] Added unit test for ButtonDanger component

---
+++ src/beinComponents/Button/ButtonDanger.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {renderWithRedux, cleanup, fireEvent} from '~/test/testUtils';
+
+import ButtonDanger from './ButtonDanger';
+
+afterEach(cleanup);
+
+describe('ButtonDanger conponent', () => {
+  const children = 'Button Danger';
+
+  const testID = 'button_danger.test';
+
+  it('renders correctly', () => {
+    const rendered = renderWithRedux(
+      <ButtonDanger testID={testID}>{children}</ButtonDanger>,
+    ).toJSON();
+
+    expect(rendered).toMatchSnapshot();
+  });
+
+  it('renders children correctly', () => {
+    const {getByTestId} = renderWithRedux(
+      <ButtonDanger testID={testID}>{children}</ButtonDanger>,
+    );
+    const component = getByTestId(testID);
+
+    expect(component.props.children[0].props.children[2].props.children).toBe(
+      children,
+    );
+  });
+
+  it('renders style correctly', () => {
+    const {getByTestId} = renderWithRedux(
+      <ButtonDanger style={{margin: 12}} testID={testID}>
+        {children}
+      </ButtonDanger>,
+    );
+
+    const component = getByTestId(testID);
+
+    expect(component.props.style.margin).toBe(12);
+  });
+
+  it('renders disabled correctly', () => {
+    const onPress = jest.fn();
+    const {getByTestId} = renderWithRedux(
+      <ButtonDanger disabled onPress={onPress} testID={testID}>
+        {children}
+      </ButtonDanger>,
+    );
+
+    const component = getByTestId(testID);
+    fireEvent.press(component);
+    expect(onPress).not.toBeCalled();
+  });
+});
