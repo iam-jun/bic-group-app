b98e242d19a125ff7f2efdd1d973906d9757312d
NamAnh <namanh@tgm.vn>
2022-02-24 08:47:35 +0700

    [BEIN-4426] handle edit comment photo

--- src/screens/Post/CreateComment/index.tsx
+++ src/screens/Post/CreateComment/index.tsx
@@ -1,5 +1,5 @@
-import React, {FC, useRef, useEffect} from 'react';
-import {Keyboard, StyleSheet} from 'react-native';
+import React, {FC, useRef, useEffect, useState} from 'react';
+import {Keyboard, ScrollView, StyleSheet} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
 
@@ -8,7 +8,12 @@ import {ITheme} from '~/theme/interfaces';
 import {useKeySelector} from '~/hooks/selector';
 import postKeySelector from '~/screens/Post/redux/keySelector';
 import postActions from '~/screens/Post/redux/actions';
-import {IActivityData, IReaction} from '~/interfaces/IPost';
+import {
+  IActivityData,
+  IActivityDataImage,
+  ICreatePostImage,
+  IReaction,
+} from '~/interfaces/IPost';
 import * as modalActions from '~/store/modal/actions';
 import {useRootNavigation} from '~/hooks/navigation';
 
@@ -17,6 +22,10 @@ import ScreenWrapper from '~/beinComponents/ScreenWrapper';
 import PostInput from '~/beinComponents/inputs/PostInput';
 import i18n from '~/localization';
 import _MentionInput from '~/beinComponents/inputs/_MentionInput';
+import CommentToolbar from '~/screens/Post/components/CommentToolbar';
+import ImagePicker from '~/beinComponents/ImagePicker';
+import UploadingImage from '~/beinComponents/UploadingImage';
+import {getResourceUrl, uploadTypes} from '~/configs/resourceConfig';
 
 export interface CreateCommentProps {
   route?: {
@@ -28,39 +37,69 @@ export interface CreateCommentProps {
 }
 
 const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
+  const [uploading, setUploading] = useState(false);
+  const [selectedImg, setSelectedImg] = useState<ICreatePostImage>();
+
   const mentionInputRef = useRef<any>();
   const {commentId, groupIds} = route?.params || {};
 
   const dispatch = useDispatch();
   const {rootNavigation} = useRootNavigation();
   const theme = useTheme() as ITheme;
+  const {dimension} = theme;
   const {colors} = theme;
   const styles = createStyle();
 
   const comment: IReaction =
     useKeySelector(postKeySelector.commentById(commentId)) || {};
   const oldContent = comment?.data?.content;
+  const oldImages = comment?.data?.images;
 
   const loading = useKeySelector(postKeySelector.createComment.loading);
   const content = useKeySelector(postKeySelector.createComment.content);
+  const image = useKeySelector(postKeySelector.createComment.image);
 
-  const isEditHasChange = content !== oldContent;
-  const disableButton = !content || content === oldContent || loading;
+  const isContentHasChange = content !== oldContent;
+  const isImageHasChange =
+    selectedImg?.fileName !== oldImages?.[0]?.origin_name;
+  const isEditHasChange = isImageHasChange || isContentHasChange;
+
+  const disableButton =
+    (!isContentHasChange && !isImageHasChange) || loading || uploading;
+  const showToolbar = !selectedImg;
 
   useEffect(() => {
-    if (oldContent) {
-      dispatch(postActions.setCreateComment({content: oldContent}));
-    }
+    dispatch(
+      postActions.setCreateComment({
+        content: oldContent || '',
+        image: oldImages?.[0],
+      }),
+    );
     if (oldContent && !mentionInputRef?.current?.getContent?.()) {
       mentionInputRef?.current?.setContent?.(oldContent);
     }
-  }, [oldContent]);
+    if (oldImages?.[0]) {
+      const {name, origin_name, width, height} = oldImages[0];
+      const file: any = {width: width || 1, height: height || 1};
+      setSelectedImg({
+        url: getResourceUrl(uploadTypes.commentImage, name),
+        fileName: origin_name,
+        file,
+      });
+    }
+  }, [oldContent, oldImages]);
 
-  const onPressComment = () => {
+  const onPressSave = () => {
     Keyboard.dismiss();
     if (commentId && comment) {
-      const newData: IActivityData = {};
-      newData.content = content;
+      const imageData: IActivityDataImage = {
+        name: selectedImg?.url || selectedImg?.fileName || '',
+        origin_name: selectedImg?.fileName,
+        width: selectedImg?.file?.width,
+        height: selectedImg?.file?.height,
+      };
+      const newData: IActivityData = {content, images: [imageData]};
+      console.log(`\x1b[35m🐣️ index onPressSave `, newData, `\x1b[0m`);
       dispatch(
         postActions.putEditComment({
           id: commentId,
@@ -71,8 +110,17 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
     }
   };
 
+  const onSelectImage = () => {
+    ImagePicker.openPickerSingle().then(file => {
+      if (!file) return;
+      setUploading(true);
+      const image: ICreatePostImage = {fileName: file.filename, file: file};
+      setSelectedImg(image);
+    });
+  };
+
   const onChangeText = (text: string) => {
-    dispatch(postActions.setCreateComment({content: text}));
+    dispatch(postActions.setCreateComment({content: text, image}));
   };
 
   const onPressBack = () => {
@@ -96,6 +144,42 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
     rootNavigation.goBack();
   };
 
+  const onUploadImageSuccess = (url: string, filename: string) => {
+    if (selectedImg?.fileName === filename) {
+      setSelectedImg({...selectedImg, url});
+    }
+    setUploading(false);
+  };
+
+  const onRemoveImage = () => {
+    setSelectedImg(undefined);
+    setUploading(false);
+  };
+
+  const renderImage = () => {
+    if (!selectedImg) {
+      return null;
+    }
+    const {file, fileName, url} = selectedImg;
+    const {width = 1, height = 1} = file || {};
+    const ratio = height / width;
+    const dfWidth = Math.min(dimension.deviceWidth, dimension.maxNewsfeedWidth);
+
+    return (
+      <UploadingImage
+        uploadType={uploadTypes.commentImage}
+        style={{alignSelf: 'center'}}
+        file={file}
+        fileName={fileName}
+        url={url}
+        width={dfWidth}
+        height={dfWidth * ratio}
+        onUploadSuccess={onUploadImageSuccess}
+        onPressRemove={onRemoveImage}
+      />
+    );
+  };
+
   return (
     <ScreenWrapper isFullView backgroundColor={colors.background}>
       <Header
@@ -109,29 +193,33 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
           highEmphasis: true,
         }}
         onPressBack={onPressBack}
-        onPressButton={onPressComment}
+        onPressButton={onPressSave}
       />
-      <_MentionInput
-        groupIds={groupIds || ''}
-        mentionInputRef={mentionInputRef}
-        style={styles.flex1}
-        textInputStyle={styles.flex1}
-        ComponentInput={PostInput}
-        componentInputProps={{
-          modalStyle: styles.mentionInputModal,
-          value: content,
-          loading: loading,
-          isHandleUpload: true,
-          placeholder: i18n.t('post:placeholder_write_comment'),
-          onChangeText,
-        }}
-        autocompleteProps={{
-          modalPosition: 'top',
-          title: i18n.t('post:mention_title'),
-          emptyContent: i18n.t('post:mention_empty_content'),
-          modalStyle: styles.mentionInputModal,
-        }}
-      />
+      <ScrollView keyboardShouldPersistTaps={'handled'}>
+        <_MentionInput
+          groupIds={groupIds || ''}
+          mentionInputRef={mentionInputRef}
+          style={styles.flex1}
+          textInputStyle={styles.flex1}
+          ComponentInput={PostInput}
+          componentInputProps={{
+            modalStyle: styles.mentionInputModal,
+            value: content,
+            loading: loading,
+            isHandleUpload: true,
+            placeholder: i18n.t('post:placeholder_write_comment'),
+            onChangeText,
+          }}
+          autocompleteProps={{
+            modalPosition: 'top',
+            title: i18n.t('post:mention_title'),
+            emptyContent: i18n.t('post:mention_empty_content'),
+            modalStyle: styles.mentionInputModal,
+          }}
+        />
+        {renderImage()}
+      </ScrollView>
+      {showToolbar && <CommentToolbar onSelectImage={onSelectImage} />}
     </ScreenWrapper>
   );
 };
