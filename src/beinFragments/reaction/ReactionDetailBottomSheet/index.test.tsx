79ef3aeaf2970285f8448c50664e947124061ba9
ThuQuyenBein <thuquyen@tgm.vn>
2022-02-25 18:54:21 +0700

    BEIN-4727 update tc

---
+++ src/beinFragments/reaction/ReactionDetailBottomSheet/index.test.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {cleanup} from '@testing-library/react-native';
+
+import {fireEvent, renderWithRedux, configureStore} from '~/test/testUtils';
+import ReactionDetailBottomSheet from './index';
+import initialState from '~/store/initialState';
+
+afterEach(cleanup);
+
+describe('ReactionDetailBottomSheet component', () => {
+  const mockStore = configureStore([]);
+
+  const getDataPromise = jest.fn();
+  const fake_data = {
+    getDataParam: {
+      commentId: undefined,
+      postId: 'efe9c800-92be-11ec-8080-800124087730',
+    },
+    getDataPromise: getDataPromise,
+    initReaction: 'sweat_smile',
+    isOpen: true,
+    reactionCounts: {
+      comment: 9,
+      comment_count: 11,
+      grinning_face_with_star_eyes: 2,
+      kissing_closed_eyes: 2,
+      kissing_smiling_eyes: 2,
+      sweat_smile: 3,
+    },
+  };
+  it('renders correctly', () => {
+    const storeData = {...initialState};
+
+    // @ts-ignore
+    storeData.modal.reactionDetailBottomSheet = fake_data;
+    const store = mockStore(storeData);
+
+    const rendered = renderWithRedux(
+      <ReactionDetailBottomSheet />,
+      store,
+    ).toJSON();
+    expect(rendered).toMatchSnapshot();
+  });
+});
--- src/test/setup.js
+++ src/test/setup.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import * as ReactNative from 'react-native';
 import {configure} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -37,15 +38,20 @@ jest.doMock('i18next', () => ({
   t: str => str,
 }));
 
-jest.doMock('react-native-paper', () => ({
-  useTheme: () => {
-    return {
+jest.doMock('react-native-paper', () => {
+  const RealModule = jest.requireActual('react-native-paper');
+  const MockedModule = {
+    ...RealModule,
+    // eslint-disable-next-line react/prop-types
+    Portal: ({children}) => <ReactNative.View>{children}</ReactNative.View>,
+    useTheme: () => ({
       colors: colors.light.colors,
       spacing: spacing,
       dimension: dimension,
-    };
-  },
-}));
+  })
+  };
+  return MockedModule;
+});
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
