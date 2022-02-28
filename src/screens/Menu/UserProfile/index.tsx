b8cbe1f13149a307989886fa3ddd41ab1948218a
ThuQuyenBein <thuquyen@tgm.vn>
2022-02-28 14:50:00 +0700

    BEIN-4739 refesh newfeed if update avatar

--- src/screens/Menu/UserProfile/index.tsx
+++ src/screens/Menu/UserProfile/index.tsx
@@ -36,6 +36,7 @@ import {IFilePicked} from '~/interfaces/common';
 import ButtonWrapper from '~/beinComponents/Button/ButtonWrapper';
 import {openLink} from '~/utils/common';
 import {chatSchemes} from '~/constants/chat';
+import homeActions from '~/screens/Home/redux/actions';
 
 const UserProfile = (props: any) => {
   const {userId, params} = props?.route?.params || {};
@@ -75,6 +76,7 @@ const UserProfile = (props: any) => {
     ) {
       if (avatarState !== _avatar) {
         setAvatarState(_avatar);
+        dispatch(homeActions.getHomePosts({isRefresh: true}));
       }
       if (_bgIm !== bgImgState) {
         setBgImgState(_bgIm);
