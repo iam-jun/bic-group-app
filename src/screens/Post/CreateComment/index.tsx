17da86141720eb2c1c7b5cdbdcd8eefa9c388763
NamAnh <namanh@tgm.vn>
2022-02-28 16:29:21 +0700

    [BEIN-4426] handle input height of create comment screen

--- src/screens/Post/CreateComment/index.tsx
+++ src/screens/Post/CreateComment/index.tsx
@@ -1,7 +1,16 @@
 import React, {FC, useRef, useEffect, useState} from 'react';
-import {Keyboard, Platform, ScrollView, StyleSheet} from 'react-native';
+import {
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text as RNText,
+  View,
+  Animated,
+  Easing,
+} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
+import DeviceInfo from 'react-native-device-info';
 
 import {ITheme} from '~/theme/interfaces';
 import {useBaseHook} from '~/hooks';
@@ -11,6 +20,7 @@ import postActions from '~/screens/Post/redux/actions';
 import * as modalActions from '~/store/modal/actions';
 import {useRootNavigation} from '~/hooks/navigation';
 import {getResourceUrl, uploadTypes} from '~/configs/resourceConfig';
+import {fontFamilies} from '~/theme/fonts';
 import {
   IActivityData,
   IActivityDataImage,
@@ -28,6 +38,11 @@ import UploadingImage from '~/beinComponents/UploadingImage';
 import MentionBar from '~/beinComponents/inputs/_MentionInput/MentionBar';
 import KeyboardSpacer from '~/beinComponents/KeyboardSpacer';
 
+const inputMinHeight = 66;
+const isAndroid8 =
+  Platform.OS === 'android' && parseInt(DeviceInfo.getSystemVersion()) === 8;
+const isAnimated = isAndroid8 || Platform.OS === 'web';
+
 export interface CreateCommentProps {
   route?: {
     params?: {
@@ -41,6 +56,8 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
   const [uploading, setUploading] = useState(false);
   const [selectedImg, setSelectedImg] = useState<ICreatePostImage>();
 
+  const inputHeightAnim = new Animated.Value(inputMinHeight);
+
   const mentionInputRef = useRef<any>();
   const {commentId, groupIds} = route?.params || {};
 
@@ -50,7 +67,7 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
   const theme = useTheme() as ITheme;
   const {dimension} = theme;
   const {colors} = theme;
-  const styles = createStyle();
+  const styles = createStyle(theme);
 
   const comment: IReaction =
     useKeySelector(postKeySelector.commentById(commentId)) || {};
@@ -152,6 +169,21 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
     rootNavigation.goBack();
   };
 
+  const onLayoutCloneText = (e: any) => {
+    const height = e?.nativeEvent?.layout?.height;
+    let newHeight = Math.max(inputMinHeight, height);
+    if (isAndroid8) {
+      //Avoid bug white screen on android 8
+      newHeight = Math.min(500, newHeight);
+    }
+    Animated.timing(inputHeightAnim, {
+      toValue: newHeight,
+      useNativeDriver: false,
+      duration: 0,
+      easing: Easing.ease,
+    }).start();
+  };
+
   const onUploadImageSuccess = (url: string, filename: string) => {
     if (selectedImg?.fileName === filename) {
       setSelectedImg({...selectedImg, url});
@@ -203,31 +235,43 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
         onPressBack={onPressBack}
         onPressButton={onPressSave}
       />
-      <ScrollView keyboardShouldPersistTaps={'handled'}>
-        <_MentionInput
-          groupIds={groupIds || ''}
-          disableAutoComplete={Platform.OS !== 'web'}
-          mentionInputRef={mentionInputRef}
-          style={styles.flex1}
-          textInputStyle={styles.flex1}
-          ComponentInput={PostInput}
-          componentInputProps={{
-            modalStyle: styles.mentionInputModal,
-            value: content,
-            loading: loading,
-            isHandleUpload: true,
-            placeholder: t('post:placeholder_write_comment'),
-            onChangeText,
-          }}
-          autocompleteProps={{
-            modalPosition: 'top',
-            title: t('post:mention_title'),
-            emptyContent: t('post:mention_empty_content'),
-            modalStyle: styles.mentionInputModal,
-          }}
-        />
+      {isAnimated && (
+        <View style={styles.textCloneContainer}>
+          <RNText style={styles.textContentClone} onLayout={onLayoutCloneText}>
+            {content + '\njust invisible line'}
+          </RNText>
+        </View>
+      )}
+      <Animated.ScrollView keyboardShouldPersistTaps={'handled'}>
+        <Animated.View
+          style={{height: isAnimated ? inputHeightAnim : undefined, zIndex: 5}}>
+          <_MentionInput
+            groupIds={groupIds || ''}
+            disableAutoComplete={Platform.OS !== 'web'}
+            style={styles.flex1}
+            textInputStyle={styles.flex1}
+            mentionInputRef={mentionInputRef}
+            ComponentInput={PostInput}
+            componentInputProps={{
+              modalStyle: styles.mentionInputModal,
+              value: content,
+              loading: loading,
+              isHandleUpload: true,
+              placeholder: t('post:placeholder_write_comment'),
+              onChangeText,
+              scrollEnabled: false,
+            }}
+            autocompleteProps={{
+              modalPosition: 'bottom',
+              title: t('post:mention_title'),
+              emptyContent: t('post:mention_empty_content'),
+              showShadow: true,
+              modalStyle: {maxHeight: 350},
+            }}
+          />
+        </Animated.View>
         {renderImage()}
-      </ScrollView>
+      </Animated.ScrollView>
       {showToolbar && <CommentToolbar onSelectImage={onSelectImage} />}
       {Platform.OS !== 'web' && <MentionBar />}
       <KeyboardSpacer iosOnly />
@@ -235,7 +279,8 @@ const CreateComment: FC<CreateCommentProps> = ({route}: CreateCommentProps) => {
   );
 };
 
-const createStyle = () => {
+const createStyle = (theme: ITheme) => {
+  const {dimension, colors} = theme;
   return StyleSheet.create({
     container: {},
     flex1: {flex: 1},
@@ -245,6 +290,20 @@ const createStyle = () => {
       bottom: undefined,
       marginTop: -12,
       maxHeight: 180,
+    },
+    textContentClone: {
+      position: 'absolute',
+      top: 13,
+      left: 12,
+      right: 12,
+      opacity: 1,
+      fontSize: dimension?.sizes.body,
+      fontFamily: fontFamilies.OpenSans,
+      color: colors.success,
+    },
+    textCloneContainer: {
+      height: 0,
+      overflow: 'hidden',
     },
   });
 };
