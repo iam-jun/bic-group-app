a81be6530527c032c097cd03552b38564110f282
Huỳnh Phương Khanh <phuongkhanh@tgm.vn>
2022-03-07 16:38:46 +0700

    [BEIN-4833] Added unit test for feature join group

---
+++ src/screens/Groups/redux/saga/joinNewGroup.ts
@@ -0,0 +1,55 @@
+import i18next from 'i18next';
+import {put, call} from 'redux-saga/effects';
+
+import groupsDataHelper from '~/screens/Groups/helper/GroupsDataHelper';
+import groupsActions from '~/screens/Groups/redux/actions';
+import * as modalActions from '~/store/modal/actions';
+import {IToastMessage} from '~/interfaces/common';
+import groupJoinStatus from '~/constants/groupJoinStatus';
+import {showError} from '.';
+
+export default function* joinNewGroup({
+  payload,
+}: {
+  type: string;
+  payload: {groupId: number; groupName: string};
+}) {
+  try {
+    const {groupId, groupName} = payload;
+
+    // @ts-ignore
+    const response = yield call(groupsDataHelper.joinGroup, groupId);
+    const join_status = response?.data?.join_status;
+    const hasRequested = join_status === groupJoinStatus.requested;
+
+    if (hasRequested) {
+      yield put(groupsActions.getGroupDetail(groupId));
+      const toastMessage: IToastMessage = {
+        content: `${i18next.t('groups:text_request_join_group')} ${groupName}`,
+        props: {
+          type: 'success',
+        },
+      };
+      yield put(modalActions.showHideToastMessage(toastMessage));
+      return;
+    }
+
+    yield put(groupsActions.getJoinedGroups());
+
+    const toastMessage: IToastMessage = {
+      content: `${i18next.t(
+        'groups:text_successfully_join_group',
+      )} ${groupName}`,
+      props: {
+        type: 'success',
+      },
+    };
+
+    yield put(modalActions.showHideToastMessage(toastMessage));
+    yield put(groupsActions.setLoadingPage(true));
+    yield put(groupsActions.getGroupDetail(groupId));
+  } catch (err) {
+    console.error('joinNewGroup catch', err);
+    yield showError(err);
+  }
+}
