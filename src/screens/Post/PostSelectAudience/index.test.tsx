c851e1dc04165e82b08c07d641a2f6d55e5d7817
NamAnh <namanh@tgm.vn>
2022-03-09 11:02:31 +0700

    [BEIN-4832] add unit test post select audience call api on search

--- src/screens/Post/PostSelectAudience/index.test.tsx
+++ src/screens/Post/PostSelectAudience/index.test.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import {configureStore, renderWithRedux} from '~/test/testUtils';
-import initialState from '~/store/initialState';
+import {fireEvent, renderWithRedux} from '~/test/testUtils';
 import PostSelectAudience from '.';
+import postDataHelper from '~/screens/Post/helper/PostDataHelper';
 
 describe('PostSelectAudience screen', () => {
   it(`renders correctly`, async () => {
     const wrapper = renderWithRedux(<PostSelectAudience />);
     const rendered = wrapper.toJSON();
     expect(rendered).toMatchSnapshot();
+  });
+
+  it('should call get search audience when text input change', () => {
+    jest.useFakeTimers();
+    const spy = jest
+      .spyOn(postDataHelper, 'getSearchAudiences')
+      .mockImplementation(() => {
+        return Promise.resolve({data: {users: [], groups: []}});
+      });
+    const wrapper = renderWithRedux(<PostSelectAudience />);
+    const searchInput = wrapper.getByTestId('post_select_audience.search');
+    expect(searchInput).toBeDefined();
+    jest.runAllTimers();
+    fireEvent.changeText(searchInput, 'bein');
+    jest.runAllTimers();
+    expect(spy).toHaveBeenCalledWith('bein');
   });
 });
