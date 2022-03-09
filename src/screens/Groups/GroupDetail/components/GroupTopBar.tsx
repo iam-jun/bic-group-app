14f5917875596901396679a81cc502c972649665
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2022-03-09 10:58:22 +0700

    BEIN-4841 Change icon chat in group page

--- src/screens/Groups/GroupDetail/components/GroupTopBar.tsx
+++ src/screens/Groups/GroupDetail/components/GroupTopBar.tsx
@@ -108,7 +108,7 @@ const GroupTopBar = () => {
       <ButtonWrapper onPress={onPressChat}>
         <Icon
           testID="group_top_bar.option_menu"
-          icon={'CommentsAlt'}
+          icon={'iconChat'}
           size={24}
           tintColor={theme.colors.iconTint}
           style={styles.iconShieldStar}
