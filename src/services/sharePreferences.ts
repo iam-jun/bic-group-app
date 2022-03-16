d688d09cbfa3c9c1dda30e6be1990899960eb9b3
Nguyen Tri Nhan <trinhan@evolgroup.vn>
2022-03-16 14:39:11 +0700

    BEIN-4692 Add save cookie for authentication on chat web

--- src/services/sharePreferences.ts
+++ src/services/sharePreferences.ts
@@ -1,13 +1,45 @@
 import {Platform} from 'react-native';
 import SharedGroupPreferences from 'react-native-shared-group-preferences';
+import {saveCookie} from '~/utils/cookie';
 import {getEnv} from '~/utils/env';
+
+type DataType = {
+  name?: string; // will save to fullname
+  email?: string;
+  avatar?: string;
+  username?: string;
+};
+
+const saveDataToCookie = (data: DataType) => {
+  const {name, email, avatar, username} = data;
+
+  if (name) {
+    saveCookie('BUSER_FULLNAME', name);
+  }
+
+  if (email) {
+    saveCookie('BUSER_EMAIL', email);
+  }
+
+  if (username) {
+    saveCookie('BUSER_USERNAME', username);
+  }
+
+  if (avatar) {
+    saveCookie('BUSER_AVATAR', avatar);
+  }
+};
 
 export const saveDataToSharedStorage = async (
   appIdentifier: string,
   key: string,
   data: any,
 ) => {
-  if (Platform.OS === 'web') return null;
+  if (Platform.OS === 'web') {
+    saveDataToCookie(data);
+    return null;
+  }
+
   try {
     await SharedGroupPreferences.setItem(key, data, appIdentifier);
   } catch (errorCode) {
@@ -20,7 +52,9 @@ export const loadFromSharedStorage = async (
   appIdentifier: string,
   key: string,
 ) => {
+  // TODO - extract data from cookie if need to do reverse flow from chat to community
   if (Platform.OS === 'web') return null;
+
   let data = null;
   try {
     data = await SharedGroupPreferences.getItem(key, appIdentifier);
@@ -33,6 +67,7 @@ export const loadFromSharedStorage = async (
 };
 
 export const saveUserToSharedPreferences = async (payload: any) => {
+  console.log('[DEBUG] saveUserToSharedPreferences', payload);
   await saveDataToSharedStorage(
     getEnv(`APP_GROUP_PACKAGE_NAME_${Platform.OS.toUpperCase()}`),
     'pref_user_info',
@@ -49,6 +84,8 @@ export const getUserFromSharedPreferences = () => {
 
 export const updateUserFromSharedPreferences = async (payload: any) => {
   const user = await getUserFromSharedPreferences();
+  console.log('[DEBUG] updateUserFromSharedPreferences', user, payload);
+
   await saveDataToSharedStorage(
     getEnv(`APP_GROUP_PACKAGE_NAME_${Platform.OS.toUpperCase()}`),
     'pref_user_info',
---
+++ src/utils/cookie.ts
@@ -0,0 +1,56 @@
+import {Platform} from 'react-native';
+import {getEnv} from './env';
+
+/**
+ * Do not change
+ * This cookie names are shared with chat web
+ */
+const COOKIE_DICTIONARY = {
+  BUSER_USERNAME: 'BUSER_USERNAME',
+  BUSER_FULLNAME: 'BUSER_FULLNAME',
+  BUSER_EMAIL: 'BUSER_EMAIL',
+  BUSER_AVATAR: 'BUSER_AVATAR',
+};
+
+type CookieNameType = keyof typeof COOKIE_DICTIONARY;
+
+const isOnHTTPS = () => {
+  return getEnv('SELF_DOMAIN')?.includes('https');
+};
+
+/**
+ * As cookies' name is different between localhost and other environments,
+ * we need to get the correct cookie name via this function
+ */
+const getCookieName = (cookie: CookieNameType) => {
+  let cookiePrefix = '';
+  if (isOnHTTPS()) {
+    cookiePrefix = '__Secure-';
+  }
+
+  return `${cookiePrefix}${cookie}`;
+};
+
+export const saveCookie = (cookie: CookieNameType, value: string) => {
+  const cookieName = getCookieName(cookie);
+
+  let secureAttribute = '';
+  if (isOnHTTPS()) {
+    secureAttribute = 'Secure';
+  }
+
+  document.cookie = `${cookieName}=${value}; Domain=${window.location.hostname}; SameSite=strict; ${secureAttribute}`;
+};
+
+export const clearUserCookies = () => {
+  if (Platform.OS !== 'web') return;
+
+  const cookieList = Object.values(COOKIE_DICTIONARY) as CookieNameType[];
+
+  cookieList.forEach(cookie => {
+    const cookieName = getCookieName(cookie);
+
+    document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
+    document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=/`;
+  });
+};
