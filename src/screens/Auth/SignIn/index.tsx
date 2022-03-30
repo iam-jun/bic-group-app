4d8ca7c11140f1746b466f8a2dbb036dbef9a44b
Diep Lam Minh Thu <dieplamminhthu@evolgroup.vn>
2022-03-30 10:59:44 +0700

    [BEIN-4587][Chat][App][Authen] Update Authen Flow - Remove log

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -118,7 +118,6 @@ const SignIn = () => {
     }
 
     const isInstalled = await isAppInstalled();
-    console.log({isInstalled});
     if (isInstalled) {
       const user = await getUserFromSharedPreferences();
       setValue('email', user?.email);
