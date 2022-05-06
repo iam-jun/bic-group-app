8b6895dcee5483eb9f15979b3783736870efeca3
Huynh Phuong Khanh <phuongkhanh@tgm.vn>
2022-05-06 11:54:44 +0700

    [BEIN-5515] Updated test cases for PageContent and PrivateWelcome

--- src/screens/Groups/CommunityDetail/components/PageContent.test.tsx
+++ src/screens/Groups/CommunityDetail/components/PageContent.test.tsx
@@ -4,23 +4,30 @@ import {renderWithRedux} from '~/test/testUtils';
 import PageContent from './PageContent';
 
 describe('PageContent', () => {
+  const onScroll = jest.fn();
+  const onButtonLayout = jest.fn();
+
   it('should render InfoHeader correctly', () => {
-    const wrapper = renderWithRedux(<PageContent />);
+    const wrapper = renderWithRedux(
+      <PageContent onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const infoHeaderComp = wrapper.getByTestId('info_header');
     expect(infoHeaderComp).toBeDefined();
   });
 
   it('should render About button correctly', () => {
-    const wrapper = renderWithRedux(<PageContent />);
+    const wrapper = renderWithRedux(
+      <PageContent onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const aboutBtn = wrapper.getByTestId('page_content.about_btn');
     expect(aboutBtn).toBeDefined();
-    expect(wrapper).toMatchSnapshot();
   });
 
   it('should render Members button correctly', () => {
-    const wrapper = renderWithRedux(<PageContent />);
+    const wrapper = renderWithRedux(
+      <PageContent onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const memberBtn = wrapper.getByTestId('page_content.members_btn');
     expect(memberBtn).toBeDefined();
-    expect(wrapper).toMatchSnapshot();
   });
 });
--- src/screens/Groups/CommunityDetail/components/PrivateWelcome.test.tsx
+++ src/screens/Groups/CommunityDetail/components/PrivateWelcome.test.tsx
@@ -4,20 +4,29 @@ import {renderWithRedux} from '~/test/testUtils';
 import PrivateWelcome from './PrivateWelcome';
 
 describe('PrivateWelcome component', () => {
+  const onScroll = jest.fn();
+  const onButtonLayout = jest.fn();
+
   it('renders InfoHeader component correctly', () => {
-    const wrapper = renderWithRedux(<PrivateWelcome />);
+    const wrapper = renderWithRedux(
+      <PrivateWelcome onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const component = wrapper.getByTestId('info_header');
     expect(component).toBeDefined();
   });
 
   it('renders JoinCancelButton component correctly', () => {
-    const wrapper = renderWithRedux(<PrivateWelcome />);
+    const wrapper = renderWithRedux(
+      <PrivateWelcome onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const component = wrapper.getByTestId('join_cancel_button');
     expect(component).toBeDefined();
   });
 
   it('renders AboutContent component correctly', () => {
-    const wrapper = renderWithRedux(<PrivateWelcome />);
+    const wrapper = renderWithRedux(
+      <PrivateWelcome onScroll={onScroll} onButtonLayout={onButtonLayout} />,
+    );
     const component = wrapper.getByTestId('about_content');
     expect(component).toBeDefined();
   });
