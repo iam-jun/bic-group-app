64e1e3d2e821b16d6cfb9d4a861dfd7a75ebb335
ThuQuyenBein <thuquyen@tgm.vn>
2022-05-12 10:22:55 +0700

    BEIN-5615 update

--- src/screens/Post/components/postView/PostViewFooter.tsx
+++ src/screens/Post/components/postView/PostViewFooter.tsx
@@ -28,7 +28,7 @@ const validateReactionCount = (reactionCounts: any) => {
   let count = 0;
   Object.values(reactionCounts || {})?.map((reaction: any) => {
     const key = Object.keys(reaction || {})?.[0];
-    if (!!key && !!reaction?.[key]) {
+    if (!!key && !!reaction?.[key] && !blacklistReactions?.[key]) {
       count++;
     }
   });
