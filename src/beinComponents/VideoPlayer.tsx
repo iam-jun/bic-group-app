0c5c1f98e44c4084155c2bb9b4b490b7d28b6494
NamAnh <namanh@tgm.vn>
2022-06-05 21:56:46 +0700

    [BEIN-6195] add react-native-video

--- src/beinComponents/VideoPlayer.tsx
+++ src/beinComponents/VideoPlayer.tsx
@@ -1,14 +1,11 @@
-import React, {FC, useRef, useEffect} from 'react';
+import React, {FC} from 'react';
 import {View, StyleSheet, StyleProp, ViewStyle} from 'react-native';
 import {useTheme} from 'react-native-paper';
-// import Video from 'react-native-video';
 // @ts-ignore
-// import {default as RNVideoControls} from 'react-native-video-controls';
-import {Video, AVPlaybackStatus} from 'expo-av';
+import {default as RNVideoControls} from 'react-native-video-controls';
 
 import {ITheme} from '~/theme/interfaces';
 import {scaleSize} from '~/theme/dimension';
-import Button from '~/beinComponents/Button';
 
 export interface VideoPlayerProps {
   style?: StyleProp<ViewStyle>;
@@ -18,11 +15,7 @@ export interface VideoPlayerProps {
 const PLAYER_HEIGHT = scaleSize(232);
 
 const VideoPlayer: FC<VideoPlayerProps> = ({style, data}: VideoPlayerProps) => {
-  const video = useRef<any>(null);
-  const [status, setStatus] = React.useState<any>({});
-
   const theme = useTheme() as ITheme;
-  const {colors, spacing} = theme;
   const styles = createStyle(theme);
 
   const {url} = data || {};
@@ -31,33 +24,9 @@ const VideoPlayer: FC<VideoPlayerProps> = ({style, data}: VideoPlayerProps) => {
     return null;
   }
 
-  const onPlaybackStatusUpdate = (status: any) => {
-    setStatus(status);
-  };
-
   return (
-    <View style={styles.container}>
-      {/*<Video*/}
-      {/*  ref={video}*/}
-      {/*  style={styles.player}*/}
-      {/*  // source={{uri: url}}*/}
-      {/*  source={{*/}
-      {/*    uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',*/}
-      {/*  }}*/}
-      {/*  useNativeControls*/}
-      {/*  resizeMode={Video.RESIZE_MODE_CONTAIN}*/}
-      {/*  isLooping*/}
-      {/*  onPlaybackStatusUpdate={onPlaybackStatusUpdate}*/}
-      {/*/>*/}
-      <Button
-        onPress={() =>
-          status?.isPlaying
-            ? video.current.pauseAsync()
-            : video.current.playAsync()
-        }
-        style={{position: 'absolute', top: 0, left: 0, right: 0}}>
-        {status?.isPlaying ? 'Stop' : 'Play'}
-      </Button>
+    <View style={[styles.container, style]}>
+      <RNVideoControls style={styles.player} source={{uri: url}} paused />
     </View>
   );
 };
