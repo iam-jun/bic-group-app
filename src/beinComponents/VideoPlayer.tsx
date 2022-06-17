ec10a14854d23a44e0d1ba6007c9d7c03599cccd
ThuQuyenBein <thuquyen@tgm.vn>
2022-06-17 14:24:19 +0700

    BEIN-6578 change library from react-native-video-controls to expo-av

--- src/beinComponents/VideoPlayer.tsx
+++ src/beinComponents/VideoPlayer.tsx
@@ -1,8 +1,14 @@
-import React, {FC} from 'react';
-import {View, StyleSheet, StyleProp, ViewStyle} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {
+  View,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+  DeviceEventEmitter,
+  PixelRatio,
+} from 'react-native';
 import {useTheme} from 'react-native-paper';
-// @ts-ignore
-import {default as RNVideoControls} from 'react-native-video-controls';
+import {Video, AVPlaybackStatus, ResizeMode} from 'expo-av';
 
 import {ITheme} from '~/theme/interfaces';
 import {scaleSize} from '~/theme/dimension';
@@ -17,16 +23,52 @@ const PLAYER_HEIGHT = scaleSize(232);
 const VideoPlayer: FC<VideoPlayerProps> = ({style, data}: VideoPlayerProps) => {
   const theme = useTheme() as ITheme;
   const styles = createStyle(theme);
+  const video = React.useRef(null);
+  const {url, id} = data || {};
 
-  const {url} = data || {};
+  useEffect(() => {
+    console.log('PixelRatio', PixelRatio.get());
+
+    const showBottomBarListener = DeviceEventEmitter.addListener(
+      'stopPlayingVideo',
+      (videoId: any) => {
+        if (!!videoId && videoId !== id) {
+          video.current?.setStatusAsync?.({shouldPlay: false});
+        }
+      },
+    );
+    return () => {
+      showBottomBarListener?.remove();
+    };
+  }, []);
+
+  const handlePlaybackStatusUpdate = (status: any) => {
+    if (status?.isPlaying) {
+      DeviceEventEmitter.emit('stopPlayingVideo', id);
+    }
+  };
 
   if (!url) {
     return null;
   }
 
   return (
-    <View style={[styles.container, style]}>
-      <RNVideoControls style={styles.player} source={{uri: url}} paused />
+    <View style={[styles.container]}>
+      <Video
+        ref={video}
+        source={{
+          uri: url,
+          overrideFileExtensionAndroid: 'm3u8',
+        }}
+        style={styles.player}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping={false}
+        onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
+        onError={(error: string) => {
+          console.warn('video failed', error);
+        }}
+      />
     </View>
   );
 };
@@ -35,9 +77,11 @@ const createStyle = (theme: ITheme) => {
   const {colors} = theme;
   return StyleSheet.create({
     container: {
-      backgroundColor: 'black',
-      width: '100%',
+      // width: '100%',
       height: PLAYER_HEIGHT,
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: colors.borderDisable,
     },
     player: {
       position: 'absolute',
