78c4a0bd50191c0fee3d975996ac0553a6e3d8d5
ThuQuyenBein <thuquyen@tgm.vn>
2022-07-15 10:24:12 +0700

    BEIN-7229 update PR

--- src/screens/NoInternet/components/SystemIssueModal.tsx
+++ src/screens/NoInternet/components/SystemIssueModal.tsx
@@ -17,7 +17,7 @@ const SystemIssueModal = () => {
 
   const systemIssue = useKeySelector(noInternetKeySelector.systemIssue);
   const optionsStyle = useAnimatedStyle(() => ({
-    opacity: withTiming(systemIssue, {duration: 500}),
+    opacity: withTiming(systemIssue ? 1 : 0, {duration: 500}),
   }));
 
   if (!systemIssue) return null;
