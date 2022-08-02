f898fd87565518c23258b86d7f57796383b293e5
Diep Lam Minh Thu <dieplamminhthu@evolgroup.vn>
2022-08-02 15:15:37 +0700

    BEIN-7481 Apply zustand to hanndle chat event with immer

--- src/store/chat/index.ts
+++ src/store/chat/index.ts
@@ -1,12 +1,16 @@
 import { AxiosResponse } from 'axios';
 import apiConfig from '~/configs/apiConfig';
+import chatSocketEvents from '~/constants/chatSocketEvents';
 import { makeHttpRequest } from '~/services/httpApiRequest';
-import { createStore, withDevtools, withPersist } from '../utils';
+import {
+  createStore, withDevtools, withImmer, withPersist,
+} from '../utils';
+import { handleChannelViewedEvent, handlePostedEvent, handlePostUnreadEvent } from './utils';
 
 export interface ChatState {
   unreadChannels: any,
   initChat: () => void,
-  // setUnreadChannels: (payload: any) => void
+  handleChatEvent: (userId: string, payload: any) => void
 }
 
 const useChatStore = (set, get) => ({
@@ -30,6 +34,37 @@ const useChatStore = (set, get) => ({
       console.error('initChat', error)
     }
   },
+  handleChatEvent: (userId: string, payload: any) => {
+    let channel = null;
+    const { unreadChannels } = get()
+    switch (payload.event) {
+      case chatSocketEvents.POSTED:
+        channel = handlePostedEvent(payload, unreadChannels, userId);
+        break;
+      case chatSocketEvents.CHANNEL_VIEWED:
+        channel = handleChannelViewedEvent(payload);
+        break;
+      case chatSocketEvents.POST_UNREAD:
+        channel = handlePostUnreadEvent(payload);
+        break;
+      default:
+        break;
+    }
+    if (channel?.id) {
+      // with immer
+      set((state) => {
+        state.unreadChannels[channel.id] = channel;
+      });
+    }
+
+    // without immer
+    // set({
+    //   unreadChannels: {
+    //     ...unreadChannels,
+    //     ...channel,
+    //   },
+    // })
+  },
 });
 
-export default createStore<ChatState>(withDevtools(withPersist(useChatStore, { name: 'chat-store' })));
+export default createStore<ChatState>(withDevtools(withImmer(withPersist(useChatStore, { name: 'chat-store' }))));
---
+++ src/store/chat/utils.ts
@@ -0,0 +1,31 @@
+import { parseSafe } from '~/utils/common';
+
+export const handlePostedEvent = (payload: any, unreadChannels:any, userId: string) => {
+  const mentions = parseSafe(payload.data.mentions);
+  const post = parseSafe(payload.data.post);
+
+  if (mentions.includes(userId) && !post.root_id) {
+    const id = payload.broadcast.channel_id;
+    let channel = unreadChannels[id];
+    if (!channel) {
+      channel = { id, mentionCountRoot: 1 };
+    } else {
+      channel = { id, mentionCountRoot: channel.mentionCountRoot + 1 };
+    }
+    return channel;
+  }
+  return null;
+}
+
+export const handleChannelViewedEvent = (payload: any) => {
+  const id = payload.data.channel_id;
+  const channel = { id, mentionCountRoot: 0 };
+  return channel;
+}
+
+export const handlePostUnreadEvent = (payload: any) => {
+  const id = payload.broadcast.channel_id;
+  const channel = { id, mentionCountRoot: payload.data.mention_count_root };
+
+  return channel;
+}
