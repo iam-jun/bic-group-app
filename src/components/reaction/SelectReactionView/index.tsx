1774b0dd4cdfcb6a9b51e7797b0010528acb8155
Minh Thu <>
2022-08-22 16:39:29 +0700

    [BEIN-7594] Update reaction style

--- src/components/reaction/SelectReactionView/index.tsx
+++ src/components/reaction/SelectReactionView/index.tsx
@@ -5,23 +5,26 @@ import {
 } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Button from '~/beinComponents/Button';
-import { margin } from '~/theme/spacing';
+import { margin, padding } from '~/theme/spacing';
 import Tab from '~/baseComponents/Tab';
 import { ANIMATED_EMOJI, STATIC_EMOJI } from '~/resources/emoji';
 import { useBaseHook } from '~/hooks';
+import { dimension } from '~/theme';
 
 export interface ReactionViewProps {
   onPressReaction: (key: string) => void;
 }
 
-const ITEM_SIZE = 40;
 const MAX_ROWS = 5;
+const NUM_COLUMNS = 8;
 
 const SelectReactionView: FC<ReactionViewProps> = ({
   onPressReaction,
 }: ReactionViewProps) => {
   const { t } = useBaseHook();
   const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const ITEM_SIZE = dimension.deviceWidth / NUM_COLUMNS;
+  const EMOJI_SIZE = ITEM_SIZE - padding.small * 2;
 
   // Initial rows = 4
   const [contentHeight, setContentHeight] = React.useState(ITEM_SIZE * 4);
@@ -44,10 +47,10 @@ const SelectReactionView: FC<ReactionViewProps> = ({
     <Button
       key={`select_reaction_${item}`}
       testID={`select_reaction_view.item_${item}`}
-      style={styles.item}
+      style={[styles.item, { width: ITEM_SIZE, height: ITEM_SIZE }]}
       onPress={() => _onPressReaction(item)}
     >
-      <Image style={styles.icon} resizeMode="contain" source={data[item]} />
+      <Image style={[styles.icon, { width: EMOJI_SIZE }]} resizeMode="contain" source={data[item]} />
     </Button>
   );
 
@@ -70,7 +73,7 @@ const SelectReactionView: FC<ReactionViewProps> = ({
         onPressTab={onPressTab}
       />
       <View
-        style={[styles.tabContainter, { height: contentHeight }]}
+        style={[styles.tabContainter, { height: contentHeight, maxHeight: ITEM_SIZE * MAX_ROWS }]}
         onLayout={onLayout}
       >
         <ScrollView style={styles.list}>
@@ -90,7 +93,6 @@ const styles = StyleSheet.create({
   },
   tabContainter: {
     flex: 1,
-    maxHeight: ITEM_SIZE * MAX_ROWS,
   },
   list: {
     marginTop: margin.tiny,
@@ -100,13 +102,11 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap', // Needed for wrapping for the items
   },
   item: {
-    width: ITEM_SIZE,
-    height: ITEM_SIZE,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: padding.small,
   },
   icon: {
-    width: 24,
     aspectRatio: 1,
   },
 });
