bea9ddf9c953e3486808b6950c8fd7a806aa2f5d
ThuQuyenBein <thuquyen@tgm.vn>
2023-01-12 13:49:20 +0700

    BEIN-11481 handle upload upsupported file

--- src/screens/post/CreatePost/hooks/useCreatePost.ts
+++ src/screens/post/CreatePost/hooks/useCreatePost.ts
@@ -91,7 +91,7 @@ const useCreatePost = ({ screenParams, mentionInputRef }: IUseCreatePost) => {
 
   const { images, imageUploading } = validateImages(selectingImages, t);
   const { video, videoUploading } = validateVideo(selectingVideo, t);
-  const { files, fileUploading } = validateFiles(selectingFiles, t);
+  const { files, fileUploading, fileError } = validateFiles(selectingFiles, t);
 
   // const [hasVideoProgress, setHasVideoProgress] = useState(videoUploading);
 
@@ -185,6 +185,7 @@ const useCreatePost = ({ screenParams, mentionInputRef }: IUseCreatePost) => {
     = imageUploading
     || videoUploading
     || fileUploading
+    || fileError
     || content?.trim?.()?.length === 0
     || chosenAudiences.length === 0
     || isLoadingLinkPreview
