8e25fc98e35809064e7f97ea1d58737eed9bb6ac
Lê Sĩ Sơn <sison@evol.vn>
2023-03-09 08:33:49 +0000

    Merged in BEIN-12899-Add-icon-tags-in-front-of-tags (pull request #2650)
    BEIN-12899-Add-icon-tags-in-front-of-tags
    
    * BEIN-12899-Add-icon-tags-in-front-of-tags
    
    
    Approved-by: Nam Anh Trần

--- src/components/TagsView/index.tsx
+++ src/components/TagsView/index.tsx
@@ -1,6 +1,9 @@
+import { ExtendedTheme, useTheme } from '@react-navigation/native';
 import React, { FC } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Icon from '~/baseComponents/Icon';
 import { ITag } from '~/interfaces/ITag';
+import { spacing } from '~/theme';
 import TagItem from './components/TagItem';
 
 export type Props = {
@@ -9,17 +12,39 @@ export type Props = {
 };
 
 const TagsView: FC<Props> = ({ data, onPressTag }) => {
+  const theme: ExtendedTheme = useTheme();
+  const { colors } = theme;
+
   const renderItem = (item: ITag) => (
     <TagItem key={`tag_${item.id}`} data={item} onPressTag={onPressTag} />
   );
 
-  return <View style={styles.container}>{data.map(renderItem)}</View>;
+  return (
+    <View style={styles.row}>
+      <Icon
+        size={18}
+        tintColor={colors.neutral20}
+        icon="Tag"
+        style={styles.iconTag}
+      />
+      <View style={styles.container}>{data.map(renderItem)}</View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+  },
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    flex: 1,
+  },
+  iconTag: {
+    marginRight: spacing.margin.small,
+    marginTop: spacing.margin.base,
   },
 });
 
