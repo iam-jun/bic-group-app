f89a3157cf152c3aae76d3863629a5b2f70be823
thanhct <thanhct@nhanlucsieuviet.com>
2023-03-27 16:06:11 +0700

    BEIN-13215 update unit test YourContent

--- src/screens/YourContent/index.test.tsx
+++ src/screens/YourContent/index.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { fireEvent, renderWithRedux } from '~/test/testUtils';
 import YourContent from './index';
+import { Dimensions } from 'react-native';
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
 describe('Your Content screen', () => {
   it('should render correctly', () => {
@@ -19,5 +23,25 @@ describe('Your Content screen', () => {
     const tabReportedContent = wrapper.queryByTestId('tab-button-your_content:title_report_content-notselected');
     expect(tabReportedContent).toBeDefined();
     fireEvent.press(tabReportedContent);
+  });
+
+  it('should call hide() when scroll down', () => {
+    const wrapper = renderWithRedux(
+      <YourContent />,
+    );
+
+    
+
+    const flatList = wrapper.getByTestId('draft_post.list');
+    expect(flatList).toBeDefined();
+
+    fireEvent.scroll(flatList, {
+      nativeEvent: {
+        contentSize: { height: 600, width: 400 },
+        contentOffset: { y: 150, x: 0 },
+        layoutMeasurement: { height, width },
+      }
+    });
+
   });
 });
