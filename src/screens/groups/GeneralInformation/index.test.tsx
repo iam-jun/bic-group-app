c81738cf91318e2bfa780bc146b11d519ec0e1c9
thanhct <thanhct@nhanlucsieuviet.com>
2023-03-29 11:56:14 +0700

    BEIN-13480 handle case fail in GeneralInfomation

--- src/screens/groups/GeneralInformation/index.test.tsx
+++ src/screens/groups/GeneralInformation/index.test.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import * as redux from 'react-redux';
 import MockedNavigator from '~/test/MockedNavigator';
 import { groupDetailData } from '~/test/mock_data/group';
+import { myPermissionsData } from '~/test/mock_data/myPermissions';
 import { fireEvent, renderWithRedux } from '~/test/testUtils';
 import GeneralInformation from '.';
 import * as helper from './helper';
+import useMyPermissionsStore from '~/store/permissions';
 
 describe('GeneralInformation component', () => {
   const component = () => (
@@ -25,32 +27,46 @@ describe('GeneralInformation component', () => {
   });
 
   it('should call onEditAvatar when press edit avatar', () => {
+    useMyPermissionsStore.setState((state) => {
+      state.data = myPermissionsData;
+      return state;
+    });
+
     const _openImagePicker = jest.spyOn(helper, '_openImagePicker');
     const rendered = renderWithRedux(<MockedNavigator component={component} />);
     const avatarComponent = rendered.getByTestId('general_information.avatar');
-    const button = avatarComponent.findByProps({ testID: 'avatar.button_edit' });
+    const button = avatarComponent.findByProps({ testID: 'info_card.button_edit' });
     fireEvent.press(button);
-    expect(_openImagePicker).toBeCalledWith(
-      mockDispatchFn,
-      groupDetailData.group.id,
-      'icon',
-      'group_avatar',
-      'group',
-    );
+
+    expect(_openImagePicker).toBeCalledWith({
+      dispatch: mockDispatchFn,
+      id: groupDetailData.group.id,
+      fieldName: 'icon',
+      uploadType: 'group_avatar',
+      destination: 'group',
+      rootGroupId: groupDetailData.group.id, 
+    });
   });
 
   it('should call onEditCover when press edit cover', () => {
+    useMyPermissionsStore.setState((state) => {
+      state.data = myPermissionsData;
+      return state;
+    });
+
     const _openImagePicker = jest.spyOn(helper, '_openImagePicker');
     const rendered = renderWithRedux(<MockedNavigator component={component} />);
     const coverComponent = rendered.getByTestId('general_information.cover');
-    const button = coverComponent.findByProps({ testID: 'cover.button_edit' });
+    const button = coverComponent.findByProps({ testID: 'info_card.button_edit' });
     fireEvent.press(button);
-    expect(_openImagePicker).toBeCalledWith(
-      mockDispatchFn,
-      groupDetailData.group.id,
-      'backgroundImgUrl',
-      'group_cover',
-      'group',
-    );
+
+    expect(_openImagePicker).toBeCalledWith({
+      dispatch: mockDispatchFn,
+      id: groupDetailData.group.id,
+      fieldName: 'backgroundImgUrl',
+      uploadType: 'group_cover',
+      destination: 'group',
+      rootGroupId: groupDetailData.group.id, 
+    });
   });
 });
