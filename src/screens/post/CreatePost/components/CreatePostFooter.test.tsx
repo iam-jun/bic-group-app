f0b4eecc3c2164782629669050a16ed80c43c628
Le Si Son <sison@evol.vn>
2023-03-29 21:44:07 +0700

    BEIN-13134-unit-test-create-post

--- src/screens/post/CreatePost/components/CreatePostFooter.test.tsx
+++ src/screens/post/CreatePost/components/CreatePostFooter.test.tsx
@@ -12,7 +12,7 @@ describe('CreatePostFooter component', () => {
   const onPressTags = jest.fn();
   const onPressSeries = jest.fn();
 
-  it('renders correctly mention bar with items', async () => {
+  it('given mention users should show mention bar with items', async () => {
     useMentionInputStore.setState((state) => {
       state.data = [MENTION_USER];
       return state;
@@ -25,7 +25,24 @@ describe('CreatePostFooter component', () => {
       onPressTags={onPressTags}
       onPressSeries={onPressSeries}
     />, store);
-    const items = wrapper.getAllByTestId('mention_bar.item');
+    const items = wrapper.queryAllByTestId('mention_bar.item');
     expect(items?.length).toBe(1);
+  });
+
+  it('given mention users = empty should show mention bar with no items', async () => {
+    useMentionInputStore.setState((state) => {
+      state.data = [];
+      return state;
+    });
+    const storeData = { ...initialState };
+    const store = mockStore(storeData);
+
+    const wrapper = renderWithRedux(<CreatePostFooter
+      onPressSetting={onPressSetting}
+      onPressTags={onPressTags}
+      onPressSeries={onPressSeries}
+    />, store);
+    const items = wrapper.queryAllByTestId('mention_bar.item');
+    expect(items?.length).toBe(0);
   });
 });
