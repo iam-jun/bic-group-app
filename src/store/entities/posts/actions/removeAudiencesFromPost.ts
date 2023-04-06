966fcf31d9d29206a2875a7fb4c555ed440a1c5a
thanhct <thanhct@nhanlucsieuviet.com>
2023-04-06 18:22:11 +0700

    BEIN-13519 refactor action removeAudiencesFromPost

--- src/store/entities/posts/actions/removeAudiencesFromPost.ts
+++ src/store/entities/posts/actions/removeAudiencesFromPost.ts
@@ -5,31 +5,31 @@ import showToast from '~/store/helper/showToast';
 import showToastError from '~/store/helper/showToastError';
 
 const removeAudiencesFromPost = () => async (payload: IPayloadRemoveAudiencesOfPost) => {
-    const { id, listAudiences } = payload || {};
-    if (!id || !listAudiences?.length) {
-        console.warn('\x1b[31m🐣️ action removeAudiencesFromPost: id not found or listAudiences is not an array\x1b[0m');
-        return;
+  const { id, listAudiences } = payload || {};
+  if (!id || !listAudiences?.length) {
+    console.warn('\x1b[31m🐣️ action removeAudiencesFromPost: id not found or listAudiences is not an array\x1b[0m');
+    return;
+  }
+  try {
+    const data = {
+      audience: {
+        userIds: [],
+        groupIds: listAudiences,
+      },
+    };
+    const response = await streamApi.putEditPost({
+      postId: id,
+      data,
+    });
+    if (response?.data) {
+      const post = response?.data;
+      usePostsStore.getState().actions.addToPosts({ data: post } as IPayloadAddToAllPost);
+      showToast({ content: 'post:text_deleted_audiences' });
     }
-    try {
-        const data = {
-            audience: {
-                userIds: [],
-                groupIds: listAudiences,
-            },
-        };
-        const response = await streamApi.putEditPost({
-            postId: id,
-            data,
-        });
-        if (response?.data) {
-            const post = response?.data;
-            usePostsStore.getState().actions.addToPosts({ data: post } as IPayloadAddToAllPost);
-            showToast({ content: 'post:text_deleted_audiences' });
-        }
-    } catch (e) {
-        console.error('\x1b[31m🐣️ action removeAudiencesFromPost error: ', e, '\x1b[0m');
-        showToastError(e);
-    }
-}
+  } catch (e) {
+    console.error('\x1b[31m🐣️ action removeAudiencesFromPost error: ', e, '\x1b[0m');
+    showToastError(e);
+  }
+};
 
 export default removeAudiencesFromPost;
