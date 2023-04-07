c1661d8cdf05790df41dbfe77320d5e9227b3e7f
thanhct <thanhct@nhanlucsieuviet.com>
2023-04-07 11:25:21 +0700

    BEIN-13519 add unit test

--- src/store/entities/posts/actions/removeAudiencesFromPost.test.ts
+++ src/store/entities/posts/actions/removeAudiencesFromPost.test.ts
@@ -2,90 +2,89 @@ import streamApi from '~/api/StreamApi';
 import useModalStore from '~/store/modal';
 import { act, renderHook } from '~/test/testUtils';
 import usePostsStore, { IPostsState } from '../index';
-import { ToastType } from '~/baseComponents/Toast/BaseToast';
 import { postCreatePost, responsePutEditPost } from '../__mocks__/data';
 
 describe('removeAudiencesFromPost', () => {
-    afterEach(() => {
-        jest.runOnlyPendingTimers();
-        jest.useRealTimers();
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it('should dont call api putEditPost when id or listAudiences empty', () => {
+    const response = responsePutEditPost;
+
+    const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
+      () => Promise.resolve(response) as any,
+    );
+
+    jest.useFakeTimers();
+    const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
+    act(() => {
+      result.current.actions.removeAudiencesFromPost({} as any);
     });
-    
-    it('should dont call api putEditPost when id or listAudiences empty', () => {
-        const response = responsePutEditPost;
+    expect(spy).not.toBeCalled();
+  });
 
-        const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
-            () => Promise.resolve(response) as any
-        );
+  it('should removeAudiencesFromPost success', () => {
+    const response = responsePutEditPost;
 
-        jest.useFakeTimers();
-        const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
-        act(() => {
-            result.current.actions.removeAudiencesFromPost({} as any);
-        });
-        expect(spy).not.toBeCalled();
+    const showToast = jest.fn();
+    const actions = { showToast };
+    jest.spyOn(useModalStore, 'getState').mockImplementation(() => ({ actions } as any));
+
+    const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
+      () => Promise.resolve(response) as any,
+    );
+
+    jest.useFakeTimers();
+    const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
+    act(() => {
+      result.current.actions.removeAudiencesFromPost({
+        id: responsePutEditPost.data.id,
+        listAudiences: [postCreatePost.audience.groupIds],
+      } as any);
     });
-
-    it('should removeAudiencesFromPost success', () => {
-        const response = responsePutEditPost;
-
-        const showToast = jest.fn();
-        const actions = { showToast };
-        jest.spyOn(useModalStore, 'getState').mockImplementation(() => ({ actions } as any));
-
-        const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
-            () => Promise.resolve(response) as any
-        );
-
-        jest.useFakeTimers();
-        const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
-        act(() => {
-            result.current.actions.removeAudiencesFromPost({
-                id: responsePutEditPost.data.id,
-                listAudiences: [postCreatePost.audience.groupIds],
-            } as any);
-        });
-        expect(spy).toBeCalled();
-        act(() => {
-            jest.runAllTimers();
-        });
-
-        expect(showToast).toBeCalledWith({
-            content: 'post:text_deleted_audiences',
-        });
+    expect(spy).toBeCalled();
+    act(() => {
+      jest.runAllTimers();
     });
 
-    it('should removeAudiencesFromPost failed', () => {
-        const error = 'internal error';
-
-        const showToast = jest.fn();
-        const actions = { showToast };
-        jest.spyOn(useModalStore, 'getState').mockImplementation(() => ({ actions } as any));
-
-        const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
-            () => Promise.reject(error) as any,
-        );
-
-        jest.useFakeTimers();
-
-        const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
-
-        act(() => {
-            try {
-                result.current.actions.removeAudiencesFromPost({ 
-                    id: responsePutEditPost.data.id,
-                    listAudiences: [postCreatePost.audience.groupIds],
-                } as any);
-            } catch (e) {
-                expect(e).toBeInstanceOf(TypeError);
-                expect(e).toBe(error);
-            }
-        });
-        expect(spy).toBeCalled();
-        act(() => {
-            jest.runAllTimers();
-        });
-
-        expect(showToast).toBeCalled();
+    expect(showToast).toBeCalledWith({
+      content: 'post:text_deleted_audiences',
     });
+  });
+
+  it('should removeAudiencesFromPost failed', () => {
+    const error = 'internal error';
+
+    const showToast = jest.fn();
+    const actions = { showToast };
+    jest.spyOn(useModalStore, 'getState').mockImplementation(() => ({ actions } as any));
+
+    const spy = jest.spyOn(streamApi, 'putEditPost').mockImplementation(
+      () => Promise.reject(error) as any,
+    );
+
+    jest.useFakeTimers();
+
+    const { result } = renderHook(() => usePostsStore((state: IPostsState) => state));
+
+    act(() => {
+      try {
+        result.current.actions.removeAudiencesFromPost({
+          id: responsePutEditPost.data.id,
+          listAudiences: [postCreatePost.audience.groupIds],
+        } as any);
+      } catch (e) {
+        expect(e).toBeInstanceOf(TypeError);
+        expect(e).toBe(error);
+      }
+    });
+    expect(spy).toBeCalled();
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(showToast).toBeCalled();
+  });
 });
