559e3bdb817ef1df028e080f258b147ef63af3a0
thanhct <thanhct@nhanlucsieuviet.com>
2023-04-10 10:44:06 +0700

    BEIN-13734 fix padding post content

--- src/components/PinContent/components/BoxListPinContent/components/PinPostView/PinPostBody.tsx
+++ src/components/PinContent/components/BoxListPinContent/components/PinPostView/PinPostBody.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { ExtendedTheme, useTheme } from '@react-navigation/native';
 import { IPost } from '~/interfaces/IPost';
 import PinPostImage from './PinPostImage';
 import PinPostFile from './PinPostFile';
 import PinPostVideo from './PinPostVideo';
 import PinPostContent from './PinPostContent';
 import { spacing } from '~/theme';
+import { borderRadius } from '~/theme/spacing';
+
+const WidthDevice = Dimensions.get('window').width;
+const WidthBoxBottomPadding = WidthDevice * 0.8;
 
 interface PinPostBodyProps {
     data: IPost;
@@ -14,6 +19,9 @@ interface PinPostBodyProps {
 const PinPostBody: React.FC<PinPostBodyProps> = ({ data }) => {
   const { media, content, mentions } = data || {};
   const { images, videos, files } = media || {};
+
+  const theme: ExtendedTheme = useTheme();
+  const styles = createStyles(theme);
 
   if (!content && images?.length !== 0) {
     return (
@@ -41,21 +49,43 @@ const PinPostBody: React.FC<PinPostBodyProps> = ({ data }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <PinPostContent
-        data={content}
-        mentions={mentions}
-      />
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <PinPostContent
+          data={content}
+          mentions={mentions}
+        />
+      </View>
+      <View style={styles.boxPadding}/>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: spacing.padding.large,
-    paddingBottom: spacing.padding.large,
-  },
-});
+const createStyles = (theme: ExtendedTheme) => {
+  const { colors } = theme;
+
+  return StyleSheet.create({
+    wrapper: {
+      flex: 1,
+    },
+    container: {
+      flex: 1,
+      paddingHorizontal: spacing.padding.large,
+    },
+    boxPadding: {
+      height: spacing.padding.large,
+      backgroundColor: colors.white,
+      width: WidthBoxBottomPadding,
+      borderBottomWidth: 1,
+      borderLeftWidth: 1,
+      borderRightWidth: 1,
+      borderBottomLeftRadius: borderRadius.large,
+      borderBottomRightRadius: borderRadius.large,
+      borderColor: colors.purple5,
+      marginBottom: -1.5,
+      marginLeft: -1,
+    },
+  });
+};
 
 export default PinPostBody;
