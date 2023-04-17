ede3cecce110e8bf3fa845a79582de60b0ed456b
ThuQuyenBein <thuquyen@evol.vn>
2023-04-17 11:52:38 +0700

    BEIN-13899 update UI when user is verified

--- src/screens/Menu/AccountSettings/EditBasicInfo/fragments/Gender.tsx
+++ src/screens/Menu/AccountSettings/EditBasicInfo/fragments/Gender.tsx
@@ -6,18 +6,20 @@ import { GENDER_TYPE } from '~/interfaces/IEditUser';
 
 type GenderProps = {
   genderState: GENDER_TYPE;
+  disabled?: boolean;
   setGenderState: (gender: GENDER_TYPE) => void;
 };
 
 const SPACING = 48;
 
-const Gender: FC<GenderProps> = ({ genderState, setGenderState }) => (
+const Gender: FC<GenderProps> = ({ genderState, disabled, setGenderState }) => (
   <View style={styles.row}>
     <Radio
       useI18n
       testID="settings.male"
       label="settings:text_male"
       isChecked={genderState === GENDER_TYPE.MALE}
+      disabled={disabled ? genderState === GENDER_TYPE.MALE ? 'disabled-auto-selected' : 'disabled' : undefined}
       onPress={() => setGenderState(GENDER_TYPE.MALE)}
     />
     <ViewSpacing width={SPACING} />
@@ -26,6 +28,7 @@ const Gender: FC<GenderProps> = ({ genderState, setGenderState }) => (
       testID="settings.female"
       label="settings:text_female"
       isChecked={genderState === GENDER_TYPE.FEMALE}
+      disabled={disabled ? genderState === GENDER_TYPE.FEMALE ? 'disabled-auto-selected' : undefined : 'disabled'}
       onPress={() => setGenderState(GENDER_TYPE.FEMALE)}
     />
     <ViewSpacing width={SPACING} />
@@ -34,6 +37,7 @@ const Gender: FC<GenderProps> = ({ genderState, setGenderState }) => (
       testID="settings.others"
       label="settings:text_others"
       isChecked={genderState === GENDER_TYPE.OTHERS}
+      disabled={disabled ? genderState === GENDER_TYPE.OTHERS ? 'disabled-auto-selected' : 'disabled' : undefined}
       onPress={() => setGenderState(GENDER_TYPE.OTHERS)}
     />
   </View>
