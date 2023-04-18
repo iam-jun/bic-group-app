767fe872a944971cbc468f97e186ba39b076dfbc
ThuQuyenBein <thuquyen@evol.vn>
2023-04-18 11:48:59 +0700

    BEIN-13895 handle noti after approving kyc

--- src/beinComponents/list/items/NotificationItem/NotificationContent/index.tsx
+++ src/beinComponents/list/items/NotificationItem/NotificationContent/index.tsx
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ExtendedTheme, useTheme } from '@react-navigation/native';
 
 import MarkdownView from '~/beinComponents/MarkdownView';
 import TimeView from '~/beinComponents/TimeView';
 import spacing from '~/theme/spacing';
+import Icon from '~/baseComponents/Icon';
+import { NOTIFICATION_TYPE } from '~/constants/notificationTypes';
+
+const LIST_NOTI_SHOW_ICON = [
+  {
+    type: NOTIFICATION_TYPE.APPROVED_KYC,
+    icon: 'BadgeCheck',
+  },
+];
 
 interface Props {
   description: string;
   content: string;
   updatedAt: string;
-
+  type?: string;
 }
 
 const NotificationContent = ({
   description,
   content,
   updatedAt,
+  type = '',
 }: Props) => {
   const theme: ExtendedTheme = useTheme();
   const { colors } = theme;
 
+  const [icon, setIcon] = useState('');
+
+  useEffect(() => {
+    if (type && !icon) {
+      const notiIconInfo = LIST_NOTI_SHOW_ICON.find((item) => item.type === type);
+      if (notiIconInfo) {
+        setIcon(notiIconInfo.icon);
+      }
+    }
+  }, [type, icon]);
+
   return (
     <View testID="notification_content" style={styles.container}>
-      <MarkdownView testID="notification_content.description">
-        {description}
-      </MarkdownView>
+      <View style={styles.row}>
+        {!!icon
+      && (
+      <Icon
+        testID="notification_content.icon"
+        size={14}
+        tintColor={colors.green50}
+        icon={icon as any}
+        style={styles.icon}
+      />
+      )}
+        <MarkdownView testID="notification_content.description">
+          {description}
+        </MarkdownView>
+      </View>
       {!!content && (
         <MarkdownView
           testID="notification_content.content"
@@ -50,6 +83,13 @@ const styles = StyleSheet.create({
   },
   timeCreated: {
     marginTop: spacing.margin.tiny,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: spacing.margin.xSmall,
   },
 });
 
