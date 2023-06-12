5878f5e06a667422f23f2ed2c362184845072ba0
thuquyen-bic <thuquyen@evol.vn>
2023-06-12 18:42:28 +0700

    BEIN-15574 add modal search and handle search text

--- src/screens/Menu/UserProfile/fragments/BadgeCollection/store/index.ts
+++ src/screens/Menu/UserProfile/fragments/BadgeCollection/store/index.ts
@@ -4,6 +4,7 @@ import IBaseState, { InitStateType } from '~/store/interfaces/IBaseState';
 import { ICommunityBadges, IUserBadge } from '~/interfaces/IEditUser';
 import getOwnedBadges from './actions/getOwnedBadges';
 import editShowingBadges from './actions/editShowingBadges';
+import searchBadges from './actions/searchBadges';
 
 export const MAX_BADGES = 3;
 
@@ -11,7 +12,9 @@ export interface IUserBadgesState extends IBaseState {
   isEditing: boolean;
   loading: boolean;
   loadingEditing: boolean;
+  loadingSearch: boolean;
   ownBadges: ICommunityBadges[];
+  dataSearch: ICommunityBadges[];
   showingBadges: IUserBadge[];
   choosingBadges: IUserBadge[];
   totalBadges: number;
@@ -25,6 +28,7 @@ export interface IUserBadgesState extends IBaseState {
     cancleSaveBadges: () => void;
     getOwnedBadges: () => void;
     editShowingBadges: () => void;
+    searchBadges: (textSearch: string) => void;
   };
 }
 
@@ -32,7 +36,9 @@ const initState: InitStateType<IUserBadgesState> = {
   isEditing: false,
   loading: true,
   loadingEditing: false,
+  loadingSearch: false,
   ownBadges: [],
+  dataSearch: [],
   showingBadges: [],
   choosingBadges: [undefined, undefined, undefined],
   error: null,
@@ -102,16 +108,18 @@ const userBadge = (set, get) => ({
       }, `removeChoosingBadges_index_${index}`);
     },
     cancleSaveBadges: () => {
-      const { showingBadges } = get();
+      const { showingBadges, ownBadges } = get();
       const choosingBadges = showingBadges?.length > 0 ? showingBadges : [undefined, undefined, undefined];
 
       set((state: IUserBadgesState) => {
         state.choosingBadges = choosingBadges;
         state.isEditing = false;
+        state.dataSearch = ownBadges;
       }, 'cancleSaveBadges');
     },
     getOwnedBadges: getOwnedBadges(set, get),
     editShowingBadges: editShowingBadges(set, get),
+    searchBadges: searchBadges(set, get),
   },
   reset: () => resetStore(initState, set),
 });
