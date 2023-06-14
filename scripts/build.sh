2dfed7d4e1ffa06ab2bf3070afe783cfc25fb015
NamAnh <namanh@evol.vn>
2023-06-14 08:37:22 +0700

    [BEIN-Config] update script build

---
+++ scripts/build.sh
@@ -0,0 +1,54 @@
+function apk() {
+    echo "Building Android app"
+    setup android $1
+    if [ "$1" = "staging" ]; then
+      fastlane android deploy_staging
+    elif [ "$1" = "prerelease" ]; then
+      fastlane android deploy_prerelease
+    elif [ "$1" = "production" ]; then
+      fastlane android deploy_production
+    else
+      echo "Invalid environment argument. Supported values are production, staging, and prerelease."
+      exit 1
+    fi
+}
+
+function ipa() {
+    echo "Building iOS app"
+    setup ios $1
+    if [ "$1" = "staging" ]; then
+      fastlane ios deploy_staging
+    elif [ "$1" = "prerelease" ]; then
+      fastlane ios deploy_prerelease
+    elif [ "$1" = "production" ]; then
+      fastlane ios deploy_production
+    else
+      echo "Invalid environment argument. Supported values are production, staging, and prerelease."
+      exit 1
+    fi
+}
+
+function setup() {
+    npm run prepare_resources $2
+    npm run clean || exit 1
+    npm install
+}
+
+case $1 in
+  apk)
+    apk $2
+  ;;
+  ipa)
+    ipa $2
+  ;;
+  *)
+    echo "Build the mobile app for Android or iOS
+    Usage: build.sh <type> [env]
+
+    Type:
+      apk   Builds Android APK(s)
+      ipa   Builds iOS IPA
+
+    Options: staging || prerelease || production"
+  ;;
+esac
---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo Cleaning started
+
+rm -rf ios/Pods
+rm -rf node_modules
+rm -rf ios/build
+rm -rf android/app/build
+watchman watch-del-all
+rm -rf /tmp/metro-cache
+npm cache clean --force
+
+echo Cleanup finished
