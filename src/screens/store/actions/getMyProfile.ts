bb1b270335e6b4e55723b2063630bbfefb8ad0a9
thuquyen-bic <thuquyen@evol.vn>
2023-06-21 11:42:49 +0700

    BEIN-15941 fix bug badge in menu header

--- src/screens/store/actions/getMyProfile.ts
+++ src/screens/store/actions/getMyProfile.ts
@@ -4,12 +4,14 @@ import showToastError from '~/store/helper/showToastError';
 import { mapProfile } from '~/helpers/common';
 import { ICommonController } from '..';
 import userApi from '~/api/UserApi';
+import { MAX_BADGES } from '~/screens/Menu/UserProfile/fragments/BadgeCollection/store';
 
-const getMyProfile = (set, _get) => async ({ userId, params }: IGetUserProfile) => {
+const getMyProfile = (set, get) => async ({ userId, params }: IGetUserProfile) => {
   try {
+    const myProfile = get() || {};
     const response = await userApi.getUserProfile(userId, params);
     const {
-      chatUserId, fullname, avatar,
+      chatUserId, fullname, avatar, showingBadges,
     } = response.data || {};
 
     await updateUserFromSharedPreferences({
@@ -18,8 +20,22 @@ const getMyProfile = (set, _get) => async ({ userId, params }: IGetUserProfile) 
       avatar,
     });
 
+    const newShowingBadges = [];
+    if (showingBadges?.length < MAX_BADGES) {
+      for (let index = 0; index < MAX_BADGES; index++) {
+        if (showingBadges?.[index]) {
+          newShowingBadges.push(showingBadges[index]);
+        } else {
+          newShowingBadges.push(undefined);
+        }
+      }
+    }
+
     set((state: ICommonController) => {
-      state.myProfile = mapProfile(response.data);
+      state.myProfile = {
+        ...mapProfile(response.data),
+        showingBadges: myProfile?.showingBadges || newShowingBadges,
+      };
     }, 'getMyProfileSuccess');
   } catch (error) {
     console.error('getMyProfile error:', error);
