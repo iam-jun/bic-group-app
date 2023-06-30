4ece251411e581aad4147f280dfe8b157f2b335f
thuquyen-bic <thuquyen@evol.vn>
2023-06-30 16:51:17 +0700

    BEIN-16343 fix description not show icon

--- src/beinComponents/list/items/NotificationItem/NotificationContent/index.tsx
+++ src/beinComponents/list/items/NotificationItem/NotificationContent/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ExtendedTheme, useTheme } from '@react-navigation/native';
 
-import MarkdownView from '~/beinComponents/MarkdownView';
 import TimeView from '~/beinComponents/TimeView';
 import spacing from '~/theme/spacing';
 import Icon from '~/baseComponents/Icon';
@@ -56,9 +55,12 @@ const NotificationContent = ({
         style={styles.icon}
       />
       )}
-        <MarkdownView testID="notification_content.description">
-          {description}
-        </MarkdownView>
+        { Boolean(description) && (
+        <Markdown
+          testID="notification_content.description"
+          value={description}
+        />
+        )}
       </View>
       {!!content && (
       <Markdown
