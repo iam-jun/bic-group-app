9a5a6f52df45ec0b34e273cc8be754bc52358d8e
thanhct <thanhtricao98@gmail.com>
2023-07-10 21:20:53 +0700

    BEIN-16497 update code

--- src/screens/YourContent/components/Quiz/store/actions/getDraftQuiz.test.ts
+++ src/screens/YourContent/components/Quiz/store/actions/getDraftQuiz.test.ts
@@ -4,100 +4,100 @@ import useDraftQuizStore, { IDraftQuizState } from '../index';
 import { postWithQuiz } from '~/test/mock_data/quiz';
 
 describe('getDraftQuiz', () => {
-    afterEach(() => {
-        jest.runOnlyPendingTimers();
-        jest.useRealTimers();
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  const response = {
+    code: 'api.ok',
+    data: {
+      list: [postWithQuiz],
+      meta: {
+        endCursor: 'eyJjcmVhdGVkQXQiOiIyMDIzLTA3LTA5VDE1OjM5OjA1LjE0MloifQ==',
+        hasNextPage: true,
+      },
+    },
+  };
+
+  it('should call api getDraftQuiz throw error', () => {
+    const error = 'internal error';
+    const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
+      () => Promise.reject(error) as any,
+    );
+
+    jest.useFakeTimers();
+
+    const { result } = renderHook(() => useDraftQuizStore((state) => state));
+
+    act(() => {
+      try {
+        result.current.actions.getDraftQuiz(true);
+      } catch (error) {
+        expect(error).toBeInstanceOf(TypeError);
+        expect(error).toBe(error);
+      }
+    });
+    expect(spy).toBeCalled();
+
+    act(() => {
+      jest.runAllTimers();
     });
 
-    const response = {
-        code: 'api.ok',
-        data: {
-            list: [postWithQuiz],
-            meta: {
-                endCursor: "eyJjcmVhdGVkQXQiOiIyMDIzLTA3LTA5VDE1OjM5OjA1LjE0MloifQ==",
-                hasNextPage: true,
-            },
-        },
-    }
+    expect(result.current.draftQuiz.refreshing).toBe(false);
+    expect(result.current.draftQuiz.loading).toBe(false);
+  });
 
-    it('should call api getDraftQuiz throw error', () => {
-        const error = 'internal error';
-        const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
-        () => Promise.reject(error) as any,
-        );
+  it('should call api getDraftQuiz when isRefresh = true success', () => {
+    const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
+      () => Promise.resolve(response),
+    );
 
-        jest.useFakeTimers();
+    jest.useFakeTimers();
 
-        const { result } = renderHook(() => useDraftQuizStore((state) => state));
+    const { result } = renderHook(() => useDraftQuizStore((state) => state));
 
-        act(() => {
-        try {
-            result.current.actions.getDraftQuiz(true);
-        } catch (error) {
-            expect(error).toBeInstanceOf(TypeError);
-            expect(error).toBe(error);
-        }
-        });
-        expect(spy).toBeCalled();
+    act(() => {
+      result.current.actions.getDraftQuiz(true);
+    });
+    expect(result.current.draftQuiz.refreshing).toBe(true);
+    expect(spy).toBeCalled();
 
-        act(() => {
-        jest.runAllTimers();
-        });
-
-        expect(result.current.draftQuiz.refreshing).toBe(false);
-        expect(result.current.draftQuiz.loading).toBe(false);
+    act(() => {
+      jest.runAllTimers();
     });
 
-    it('should call api getDraftQuiz when isRefresh = true success', () => {
-        const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
-            () => Promise.resolve(response),
-        );
+    expect(result.current.draftQuiz.data).toEqual(response.data.list);
+    expect(result.current.draftQuiz.hasNextPage).toBe(true);
+    expect(result.current.draftQuiz.refreshing).toBe(false);
+    expect(result.current.draftQuiz.loading).toBe(false);
+  });
 
-        jest.useFakeTimers();
+  it('should call api getDraftQuiz when isRefresh = false success', () => {
+    useDraftQuizStore.setState((state: IDraftQuizState) => {
+      state.draftQuiz.data = [postWithQuiz] as any;
+      return state;
+    });
+    const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
+      () => Promise.resolve(response) as any,
+    );
 
-        const { result } = renderHook(() => useDraftQuizStore((state) => state));
+    jest.useFakeTimers();
 
-        act(() => {
-            result.current.actions.getDraftQuiz(true);
-        });
-        expect(result.current.draftQuiz.refreshing).toBe(true);
-        expect(spy).toBeCalled();
+    const { result } = renderHook(() => useDraftQuizStore((state) => state));
 
-        act(() => {
-            jest.runAllTimers();
-        });
+    act(() => {
+      result.current.actions.getDraftQuiz(false);
+    });
+    expect(result.current.draftQuiz.loading).toBe(true);
+    expect(spy).toBeCalled();
 
-        expect(result.current.draftQuiz.data).toEqual(response.data.list);
-        expect(result.current.draftQuiz.hasNextPage).toBe(true);
-        expect(result.current.draftQuiz.refreshing).toBe(false);
-        expect(result.current.draftQuiz.loading).toBe(false);
+    act(() => {
+      jest.runAllTimers();
     });
 
-    it('should call api getDraftQuiz when isRefresh = false success', () => {
-        useDraftQuizStore.setState((state: IDraftQuizState) => {
-            state.draftQuiz.data = [postWithQuiz] as any;
-            return state;
-        });
-        const spy = jest.spyOn(streamApi, 'getDraftQuiz').mockImplementation(
-            () => Promise.resolve(response) as any,
-        );
-
-        jest.useFakeTimers();
-
-        const { result } = renderHook(() => useDraftQuizStore((state) => state));
-
-        act(() => {
-            result.current.actions.getDraftQuiz(false);
-        });
-        expect(result.current.draftQuiz.loading).toBe(true);
-        expect(spy).toBeCalled();
-
-        act(() => {
-            jest.runAllTimers();
-        });
-
-        expect(result.current.draftQuiz.data.length).toBe(2);
-        expect(result.current.draftQuiz.refreshing).toBe(false);
-        expect(result.current.draftQuiz.loading).toBe(false);
-    });
+    expect(result.current.draftQuiz.data.length).toBe(2);
+    expect(result.current.draftQuiz.refreshing).toBe(false);
+    expect(result.current.draftQuiz.loading).toBe(false);
+  });
 });
