e9a04850c6f098f583ba05039536896a7574f593
thuquyen-bic <thuquyen@evol.vn>
2023-08-02 14:23:13 +0700

    BEIN-17174 change post view placeholder

--- src/beinComponents/placeholder/PostViewPlaceholder.tsx
+++ src/beinComponents/placeholder/PostViewPlaceholder.tsx
@@ -4,17 +4,14 @@ import {
 } from 'react-native';
 import { ExtendedTheme, useTheme } from '@react-navigation/native';
 
-import {
-  ShineOverlay,
-  Placeholder,
-  PlaceholderLine,
-  PlaceholderMedia,
-} from 'rn-placeholder';
+import Animated from 'react-native-reanimated';
 import Image from '~/components/Image';
 import images from '~/resources/images';
 import { getRandomInt } from '~/utils/generator';
 import Divider from '~/beinComponents/Divider';
 import spacing from '~/theme/spacing';
+import { useSkeletonAnimation } from '~/hooks/useSkeletonAnimation';
+import ViewSpacing from '../ViewSpacing';
 
 export interface PostViewPlaceholderProps {
   style?: StyleProp<ViewStyle>;
@@ -29,77 +26,83 @@ const PostViewPlaceholder: FC<PostViewPlaceholderProps> = ({
 }: PostViewPlaceholderProps) => {
   const theme: ExtendedTheme = useTheme();
   const styles = createStyle(theme);
+  const animatedStyle = useSkeletonAnimation({ targetOpacityValue: 0.5, speed: 500 });
 
   return (
     <View testID={testID} style={StyleSheet.flatten([styles.container, style])}>
-      <View style={styles.importantContainer}>
+      <View style={[styles.importantContainer, styles.row]}>
+        <Animated.View style={[styles.line, animatedStyle]} />
         <Image style={styles.iconStar} source={images.ic_star_white} />
-        <Placeholder Animation={ShineOverlay} style={styles.flex1}>
-          <PlaceholderMedia style={styles.importantTitle} />
-        </Placeholder>
       </View>
-      <Placeholder
-        Animation={ShineOverlay}
-        Left={(p) => <PlaceholderMedia style={[p.style, styles.avatar]} />}
-        style={styles.infoContainer}
-      >
-        <PlaceholderLine
-          width={disableRandom ? 50 : getRandomInt(
-            30, 60,
-          )}
-          style={styles.marginBottomSmall}
-        />
-        <PlaceholderLine
-          width={disableRandom ? 60 : getRandomInt(
-            30, 80,
-          )}
-          style={styles.marginBottomSmall}
-        />
-        <PlaceholderLine
-          width={disableRandom ? 40 : getRandomInt(
-            30, 50,
-          )}
-          style={styles.margin0}
-        />
-      </Placeholder>
-      <Placeholder Animation={ShineOverlay} style={styles.contentContainer}>
+      <View style={[styles.row, styles.infoContainer]}>
+        <Animated.View style={[styles.line, styles.avatar, animatedStyle]} />
+        <View style={styles.flex1}>
+          <Animated.View
+            style={[styles.line, {
+              width: `${disableRandom ? 50 : getRandomInt(
+                30, 60,
+              )}%`,
+            }, animatedStyle]}
+          />
+          <Animated.View
+            style={[styles.line, {
+              width: `${disableRandom ? 60 : getRandomInt(
+                30, 80,
+              )}%`,
+            }, animatedStyle]}
+          />
+          <Animated.View
+            style={[styles.line, {
+              width: `${disableRandom ? 40 : getRandomInt(
+                30, 50,
+              )}%`,
+            }, animatedStyle]}
+          />
+        </View>
+      </View>
+      <View style={styles.contentContainer}>
         {Array.from(Array(disableRandom ? 3 : getRandomInt(
           1, 5,
         )).keys()).map((item) => (
-          <PlaceholderLine
+          <Animated.View
             key={`line_${item}`}
-            style={styles.marginBottomSmall}
+            style={[styles.line, {
+              width: `${disableRandom ? 80 : getRandomInt(
+                80, 100,
+              )}%`,
+            }, animatedStyle]}
           />
         ))}
-        <PlaceholderLine
-          width={disableRandom ? 45 : getRandomInt(
-            20, 80,
-          )}
-          style={styles.margin0}
+        <Animated.View
+          style={[styles.line, {
+            width: `${disableRandom ? 45 : getRandomInt(
+              20, 80,
+            )}%`,
+          }, animatedStyle]}
         />
-      </Placeholder>
-      <Placeholder Animation={ShineOverlay}>
-        <PlaceholderMedia style={styles.image} />
-      </Placeholder>
-      <View style={styles.buttonWrapper}>
-        <View style={styles.buttonContainer}>
-          <Placeholder
-            Animation={ShineOverlay}
-            style={styles.buttonContent}
-            Left={(p) => <PlaceholderMedia style={[p.style, styles.icon]} />}
-          >
-            <PlaceholderLine style={styles.margin0} height={14} />
-          </Placeholder>
+      </View>
+      <Animated.View
+        style={[styles.line, styles.image, animatedStyle]}
+      />
+      <View style={[styles.row, styles.buttonWrapper]}>
+        <View style={[styles.row, styles.buttonContainer]}>
+          <Animated.View
+            style={[styles.line, styles.icon, animatedStyle]}
+          />
+          <ViewSpacing width={spacing.margin.base} />
+          <Animated.View
+            style={[styles.line, styles.buttonContent, animatedStyle]}
+          />
         </View>
         <Divider horizontal style={styles.divider} />
-        <View style={styles.buttonContainer}>
-          <Placeholder
-            Animation={ShineOverlay}
-            style={styles.buttonContent}
-            Left={(p) => <PlaceholderMedia style={[p.style, styles.icon]} />}
-          >
-            <PlaceholderLine style={styles.margin0} height={14} />
-          </Placeholder>
+        <View style={[styles.row, styles.buttonContainer]}>
+          <Animated.View
+            style={[styles.line, styles.icon, animatedStyle]}
+          />
+          <ViewSpacing width={spacing.margin.base} />
+          <Animated.View
+            style={[styles.line, styles.buttonContent, animatedStyle]}
+          />
         </View>
       </View>
     </View>
@@ -110,18 +113,35 @@ const createStyle = (theme: ExtendedTheme) => {
   const { colors } = theme;
   return StyleSheet.create({
     flex1: { flex: 1 },
-    margin0: { marginTop: 0, marginBottom: 0 },
-    marginBottomSmall: { marginBottom: spacing.margin.small },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    line: {
+      height: spacing.margin.base,
+      borderRadius: spacing.margin.tiny,
+      backgroundColor: colors.neutral5,
+      marginVertical: spacing.margin.tiny,
+    },
+    importantContainer: {
+      backgroundColor: colors.purple1,
+      paddingVertical: spacing.padding.xTiny,
+      paddingHorizontal: spacing.padding.base,
+      justifyContent: 'space-between',
+    },
+    infoContainer: {
+      paddingTop: spacing.padding.small,
+      paddingHorizontal: spacing.padding.base,
+      paddingBottom: spacing.padding.base,
+    },
+    avatar: {
+      width: 52,
+      height: 52,
+      marginRight: 8,
+    },
     container: {
       backgroundColor: colors.white,
       marginBottom: spacing.margin.base,
-    },
-    importantContainer: {
-      backgroundColor: colors.neutral5,
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingVertical: spacing.padding.tiny,
-      paddingHorizontal: spacing.padding.base,
     },
     importantTitle: {
       marginLeft: spacing.margin.large,
@@ -129,21 +149,11 @@ const createStyle = (theme: ExtendedTheme) => {
       height: 15,
     },
     iconStar: { width: 28, height: 28 },
-    infoContainer: {
-      paddingTop: spacing.padding.small,
-      paddingHorizontal: spacing.padding.base,
-      paddingBottom: spacing.padding.base,
-    },
     content: {
       height: 40,
       marginBottom: 0,
       marginTop: 0,
       borderRadius: 100,
-    },
-    avatar: {
-      width: 52,
-      height: 52,
-      marginRight: 8,
     },
     contentContainer: {
       paddingHorizontal: spacing.padding.base,
@@ -159,17 +169,16 @@ const createStyle = (theme: ExtendedTheme) => {
       width: 16,
       height: 16,
     },
-    buttonWrapper: { height: 40, flexDirection: 'row', alignItems: 'center' },
+    buttonWrapper: { height: 40 },
     buttonContainer: {
-      flexDirection: 'row',
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center',
     },
     buttonContent: {
-      width: 100,
+      width: 80,
       justifyContent: 'center',
       alignItems: 'center',
+      height: spacing.margin.large,
     },
     divider: { height: '65%', alignSelf: 'center' },
   });
