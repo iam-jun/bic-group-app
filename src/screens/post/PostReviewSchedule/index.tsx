9bac4fc7bb1696b5e7ea4da59a98cf610a56e92d
thanhct <thanhtricao98@gmail.com>
2023-09-13 10:59:44 +0700

    BEIN-19013 refactor Schedule feature part1

---
+++ src/screens/post/PostReviewSchedule/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface PostReviewScheduleProps {
+
+}
+
+const PostReviewSchedule: React.FC<PostReviewScheduleProps> = () => {
+  
+
+
+  return (
+    <View style={styles.container}>
+      <Text>PostReviewSchedule</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default PostReviewSchedule;
